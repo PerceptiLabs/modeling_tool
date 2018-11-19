c5c10d849ee8ab2ca08bee2051413725b6615b15
Anton Stetsenko <stetsenko.ant1@gmail.com>
2018-11-19 22:03:50 +0200

    add login, registration, policy page

--- src/renderer/pages/register/register.vue
+++ src/renderer/pages/register/register.vue
@@ -3,9 +3,11 @@
     .login_logo
       img(src="~@/assets/percepti-labs-logo.svg" alt="percepti labs logo")
     .login_main
-      h1 Log In please
-      h3 Enter your Email & Password
+      h1 Get Started
+      h3 Register in 1 minute
       form.login_form
+        .form_holder
+          input(type="text" placeholder="First and Last Name")
         .form_holder
           input(type="email" placeholder="Email")
         .form_holder
@@ -13,16 +15,18 @@
         .form_holder
           base-checkbox(
 
-          ) Remember me
+          )  
+            span Agree 
+            router-link(:to="{name: 'policy'}").btn.btn--link  terms and policy
         .form_holder
           button.btn.btn--dark-blue-rev(type="button") log in
         .form_holder
-          router-link(:to="{name: 'register'}").btn.btn--link Register new account
+          router-link(:to="{name: 'login'}").btn.btn--link Already Have Account
 </template>
 
 <script>
 export default {
-  name: 'PageLogin'
+  name: 'PageRegister'
 }
 </script>
 
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
+import Vue    from 'vue'
 import Router from 'vue-router'
-import PageQuantum from '@/pages/app/quantum.vue'
-//  import PageLogin from '@/pages/login/login.vue'
+
+import PageQuantum  from '@/pages/app/quantum.vue'
+import PageLogin    from '@/pages/login/login.vue'
+import PageRegister from '@/pages/register/register.vue'
+import PagePolicy   from '@/pages/policy/policy.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  //mode: 'history',
   routes: [
-    { path: '/', name: 'quantum', component: PageQuantum },
-    //  { path: '/login', name: 'login', component: PageLogin },
+    { path: '/',          name: 'login',    component: PageLogin },
+    { path: '/register',  name: 'register', component: PageRegister },
+    { path: '/policy-page',    name: 'policy',   component: PagePolicy },
+    { path: '/app',       name: 'quantum',  component: PageQuantum },
     { path: '*', redirect: '/'
     }
   ]
