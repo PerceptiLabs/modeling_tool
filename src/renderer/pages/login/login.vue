17e9f8966b525a28057fae1d2605d64e3424c726
Anton Stetsenko <stetsenko.ant1@gmail.com>
2018-12-11 20:12:44 +0200

    upload is works

--- src/renderer/pages/login/login.vue
+++ src/renderer/pages/login/login.vue
@@ -3,7 +3,7 @@
     .login_logo
       img(src="~@/assets/percepti-labs-logo.svg" alt="percepti labs logo")
     .login_main
-      h1 Log In please
+      h1 Log In please UPDATE
       h3 Enter your Email & Password
       form.login_form
         .form_holder
