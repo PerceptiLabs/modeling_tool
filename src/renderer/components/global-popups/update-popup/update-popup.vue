fc6996cda5c602d7d66d171dc0d3f009b37a4778
Nikita V. Pastukhov <nikita.pastukhov@ssa.group>
2019-01-16 12:03:47 +0200

    added component for updates popup

--- src/renderer/components/global-popups/update-popup/update-popup.vue
+++ src/renderer/components/global-popups/update-popup/update-popup.vue
@@ -80,96 +80,9 @@ export default {
 
 <style lang="scss" scoped>
    @import '../../../scss/base';
+   @import '../../../scss/components/update-popup';
 
-  .popup-body {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background: $bg-toolbar;
-    z-index: 100;
-    width: 40rem;
-    border-radius: 0.5rem;
-    overflow: hidden;
-    padding-bottom: 1rem; 
-  }
-  .popup-body_header {
-    background: $col-txt2;
-    padding: 1rem 1.5rem;
-    display: flex;
-    justify-content: space-between;
-    font-size: 1.4rem;
-  }
-  .header_title {
-    font-size: 1.4rem;
-    color: $col-txt;
-    margin: 0;
-  }
-
-  .popup-body_info {
-    padding: 2rem 1.5rem 3rem 1.5rem;
-  }
-  .info_process-name {
-    color: $col-placeholder;
-    margin-bottom: 1rem;
-    display: block;
-    font-size: 1.2rem;
-  }
-  .info_message {
-    font-size: 1.2rem;
-  }
-
-  .info_progress-bar {
-    margin: 1rem 0;
-  }
-  .progress-bar_update-status {
-    font-size: 1.2rem;
-    display: block;
-    margin-bottom: 2rem;
-  }
-  .progress-bar_loading-line-box {
-    background:#535b71;
-    border-radius: .5rem;
-    width: 100%;
-    height: 2rem;
-
-  }
-  .progress-bar_loading-line {
-    background: linear-gradient(#73FEBB, #61E6EE);
-    width: 28%;
-    height: inherit;
-    border-radius: .5rem;
-    transition: width .5s linear;
-  }
-
-  .info_about-update-btn {
-    background: none;
-    padding: 0.5rem 0;
-    margin: 0.5rem 0;
-    text-decoration: underline;
-  }
-  .about-update-list_item {
-    margin-bottom: 1rem;
-    line-height: 1.4rem;
-    font-size: 1rem;
-
-    &:before {
-      content: '-';
-      margin-right: 0.5rem;
-    }
-  }
-
-  .popup-body_footer {
-    padding: 0 1.5rem;
-    margin-bottom: 1rem;
-    button{
-      min-width: 12rem;
-      display: inline-block;
-      height: auto;
-      padding: .65em .5em;
-      margin-right: 1rem;
-    }
-  }
+ 
 </style>
 
 
