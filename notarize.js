972fc0ca16c52c9cddef6c64d7c2da7e1b6842a3
Anton Stetsenko <stetsenko.ant1@gmail.com>
2019-09-09 16:12:44 +0300

    fix notarize

--- notarize.js
+++ notarize.js
@@ -1,9 +1,27 @@
 const notarize = require('electron-notarize').notarize;
-process.env.CSC_LINK = '../perceptilabs.p12'
-process.env.CSC_KEY_PASSWORD = 'com.perceptilabs.app'
-process.env.appleId = 'stetsenko.ant1@gmail.com'
-process.env.appleASP = 'tpms-dyrp-wfbj-xvay'
-// module.exports = async function nota(context) {
+// process.env.CSC_LINK = '../perceptilabs.p12'
+// process.env.CSC_KEY_PASSWORD = 'com.perceptilabs.app'
+// process.env.appleId = 'stetsenko.ant1@gmail.com'
+// process.env.appleASP = 'tpms-dyrp-wfbj-xvay'
+module.exports = async function nota(context) {
+    const { electronPlatformName } = context;
+    if (electronPlatformName === 'darwin') {
+        try {
+            console.log('Try notarize app');
+            await notarize({
+              appBundleId: 'com.perceptilabs.app',
+              appPath: './build/mac/PerceptiLabs.app',
+              appleId: process.env.appleId,
+              appleIdPassword: process.env.appleASP,
+            });
+            console.log('Success notarize');
+        } catch (err) {
+            console.log('Notarize app', err);
+        }
+  }
+
+};
+// async function nota() {
 //     //const { electronPlatformName } = context;
 //     //if (electronPlatformName === 'darwin') {
 //         try {
@@ -21,41 +39,4 @@ process.env.appleASP = 'tpms-dyrp-wfbj-xvay'
 //   //}
 
 // };
-async function nota() {
-    //const { electronPlatformName } = context;
-    //if (electronPlatformName === 'darwin') {
-        try {
-            console.log('Try notarize app');
-            await notarize({
-              appBundleId: 'com.perceptilabs.app',
-              appPath: './build/mac/PerceptiLabs.app',
-              appleId: process.env.appleId,
-              appleIdPassword: process.env.appleASP,
-            });
-            console.log('Success notarize');
-        } catch (err) {
-            console.log('Notarize app', err);
-        }
-  //}
-
-};
-nota();
-
-// require('dotenv').config();
-// const { notarize } = require('electron-notarize');
-
-// exports.default = async function notarizing(context) {
-//   const { electronPlatformName, appOutDir } = context;  
-//   if (electronPlatformName !== 'darwin') {
-//     return;
-//   }
-
-//   const appName = context.packager.appInfo.productFilename;
-
-//   return await notarize({
-//     appBundleId: 'com.perceptilabs.app',
-//               appPath: './dist/mac/PerceptiLabs.app',
-//               appleId: process.env.appleId,
-//               appleIdPassword: process.env.appleASP,
-//   });
-// };
+// nota();
