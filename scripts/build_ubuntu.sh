2af77ef7eff3af9de38459f9529615985fced310
Anton Kollmats <antonkollmats2@gmail.com>
2019-11-27 12:53:19 -0500

    updated build configs for linux. now produces working core build

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -38,32 +38,84 @@ mkdir frontend_out
 
 echo "Copying files files from ../../backend/"
 cd backend_tmp/
-cp -r ../../backend/* .
 
-for file in ${EXCLUDED_FILES[@]}
-do
-    echo "remove $file"
-    rm $file
-done
+cp ../../backend/*.py .
 
+mkdir analytics/
+cp ../../backend/analytics/*.py analytics/
+
+mkdir code_generator/
+cp ../../backend/code_generator/*.py code_generator/
+
+mkdir core_new/
+cp ../../backend/core_new/*.py core_new/
+
+mkdir core_new/data/
+cp ../../backend/core_new/data/*.py core_new/data/
+
+rm minicodehq.py
+rm appOc.py
+rm a2cagent.py
+rm frontend_data_code.py
+rm core_test.py
+rm serverInterface.py
+rm lwInterface.py
 
 mv setup.py setup.pyx
 mv mainServer.py mainServer.pyx
 
+mv analytics/setup.py analytics/setup.pyx
+mv code_generator/setup.py code_generator/setup.pyx
+mv core_new/setup.py core_new/setup.pyx
+mv core_new/data/setup.py core_new/data/setup.pyx
+
+mv code_generator/__init__.py code_generator/__init__.pyx
+mv core_new/data/__init__.py core_new/data/__init__.pyx
+
+# analytics
+cd analytics
+python setup.pyx develop --user
+rm *.py *.c
+mv __init__.pyx __init__.py
+rm *.pyx
+
+# code_generator
+cd ../code_generator
+mkdir code_generator
+python setup.pyx develop --user
+mv code_generator/* .
+rm -rf code_generator
+rm *.py *.c
+mv __init__.pyx __init__.py
+rm *.pyx
+
+# core_new
+cd ../core_new/
+python setup.pyx develop --user
+rm *.py *.c *.pyx
+
+# core_new.data
+cd data/
+mkdir data/
+python setup.pyx develop --user
+mv data/* .
+rm -rf data/
+rm *.py *.c
+mv __init__.pyx __init__.py
+rm *.pyx
+
+
+# root
+cd ../../
+python setup.pyx develop --user
+
+rm *.py *.c
+mv mainServer.pyx mainServer.py
+rm *.pyx
+
+
 echo "Listing files to be included in build (contents of 'backend_tmp/')"
 ls -l
-
-
-echo "Compiling..."
-python setup.pyx develop --user
-if [ $? -ne 0 ]; then exit 1; fi
-rm *.py
-mv mainServer.pyx mainServer.py
-
-echo "Listing contents of 'backend_tmp/'"
-ls -l
-
-
 
 echo "Running pyinstaller..."
 
