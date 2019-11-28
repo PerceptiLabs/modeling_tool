b6f5183b4b2977eb1085593b2d98ab73b9ed432e
Robert Lundberg <Lundbergrobert@telia.com>
2019-11-28 11:05:23 +0100

    reg query

--- scripts/setup_windows.bat
+++ scripts/setup_windows.bat
@@ -24,11 +24,14 @@ SET
 
 PATH
 
-dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.10240.0\ucrt"
-dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\ucrt"
-dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt"
-dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\ucrt"
-dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\ucrt"
+REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\Windows\v10.0" /v ProductVersion
+REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0" /v ProductVersion
+
+REM dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.10240.0\ucrt"
+REM dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.16299.0\ucrt"
+REM dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt"
+REM dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\ucrt"
+REM dir "C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\ucrt"
 
 cd ..\frontend
 call npm install
