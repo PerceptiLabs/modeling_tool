8753a41725d8b20a9b747402933727565a395529
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-02 10:54:24 +0100

    half changes to hash

--- backend/core_new/networkCache.py
+++ backend/core_new/networkCache.py
@@ -24,13 +24,22 @@ class NetworkCache():
     def to_dict(self):
         return self._dict
 
+    def _calculate_hash(self, id_, content):
+        hash_ = hash(str(content))
+        for con in content["Con"]:
+            if con in self._dict:
+                hash_+=self._dict[con].hash
+            else:
+                log.warning("Layer %s has no hash code and will be skipped in the caching for now"%str(con))
+        return hash_
+
     def needs_update(self, id_, content):
-        return self._dict[id_].hash != hash(str(content))
+        return self._dict[id_].hash != self._calculate_hash(id_, content)
 
     def update(self, id_, content, session, error):
         if id_ in self._dict:
-            log.info("Updating layer " + str(id_))
-        hash_ = hash(str(content))
+            log.info("Updating layer " + str(id_))        
+        hash_ = self._calculate_hash(id_, content)
         entry = CacheEntry(hash = hash_, session = session, error = error)
         self._dict[id_] = entry
         log.info("Cached layers: " + str(self._dict.keys()))
