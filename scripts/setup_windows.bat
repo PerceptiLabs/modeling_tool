f86303990d66e2aa0fca81f0f467a42f9c8185bb
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-03 10:12:14 +0100

    windows

--- scripts/setup_windows.bat
+++ scripts/setup_windows.bat
@@ -4,42 +4,43 @@ IF EXIST c:\tools\miniconda3 (
 )
 
 dir 
-exit 1
-choco install nodejs --yes --version 12.10.0
-choco install microsoft-visual-cpp-build-tools --version 14.0.25420.1 --yes
-REM choco install windows-sdk-10.0 --yes
-choco install miniconda3 --force --yes --params"'/AddToPath /D:c:\tools'"
+call ls
 
-REM dir C:\Program Files (x86)\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\SDK\
+REM choco install nodejs --yes --version 12.10.0
+REM choco install microsoft-visual-cpp-build-tools --version 14.0.25420.1 --yes
+REM REM choco install windows-sdk-10.0 --yes
+REM choco install miniconda3 --force --yes --params"'/AddToPath /D:c:\tools'"
 
-
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
-call vcvarsall.bat x86_amd64
-cd %VCINSTALLDIR%
-for /R %f in (*stdint.h) do set CL=-FI"%f"
-call pip install pycrypto
+REM REM dir C:\Program Files (x86)\
+REM REM dir C:\Program Files (x86)\Microsoft Visual Studio\
+REM REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\
+REM REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\
+REM REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\
+REM REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\SDK\
 
 
-SET PATH=%PATH%;C:\tools\miniconda3\Scripts
+REM cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+REM call vcvarsall.bat x86_amd64
+REM cd %VCINSTALLDIR%
+REM for /R %f in (*stdint.h) do set CL=-FI"%f"
+REM call pip install pycrypto
 
-call C:\tools\miniconda3\condabin\conda.bat init cmd.exe
-call C:\tools\miniconda3\condabin\conda.bat config --set ssl_verify no
-call C:\tools\miniconda3\condabin\conda.bat env create --force --file "..\backend\environment.yml"
-call C:\tools\miniconda3\condabin\conda.bat activate py362_
-call C:\tools\miniconda3\condabin\conda.bat env list
 
-call C:\tools\miniconda3\condabin\conda.bat list
+REM SET PATH=%PATH%;C:\tools\miniconda3\Scripts
 
-call node --version
-call npm --version
+REM call C:\tools\miniconda3\condabin\conda.bat init cmd.exe
+REM call C:\tools\miniconda3\condabin\conda.bat config --set ssl_verify no
+REM call C:\tools\miniconda3\condabin\conda.bat env create --force --file "..\backend\environment.yml"
+REM call C:\tools\miniconda3\condabin\conda.bat activate py362_
+REM call C:\tools\miniconda3\condabin\conda.bat env list
 
-REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\Windows\v10.0" /v ProductVersion
+REM call C:\tools\miniconda3\condabin\conda.bat list
 
-cd ..\frontend
-call npm install
-cd ..\scripts\ 
+REM call node --version
+REM call npm --version
+
+REM REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\Windows\v10.0" /v ProductVersion
+
+REM cd ..\frontend
+REM call npm install
+REM cd ..\scripts\ 
