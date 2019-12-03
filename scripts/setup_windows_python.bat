b477ab8f04c15df2195e67b9d53119c4997fb2b3
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-03 12:36:25 +0100

    windows

--- scripts/setup_windows_python.bat
+++ scripts/setup_windows_python.bat
@@ -18,8 +18,8 @@ REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\Sco
 
 
 set OLDPWD=%cd%
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
-call vcvarsall.bat x86_amd64
+REM cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
+call C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat x86_amd64
 cd %VCINSTALLDIR%
 for /R %f in (*stdint.h) do set CL=-FI"%f"
 call pip install pycrypto
