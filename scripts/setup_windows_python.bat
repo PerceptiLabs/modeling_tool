0efe0b3e45bf1cd59d522fc328e119ac8c7c71fa
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-03 13:59:47 +0100

    windows

--- scripts/setup_windows_python.bat
+++ scripts/setup_windows_python.bat
@@ -2,32 +2,25 @@ REM @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -I
 REM IF EXIST c:\tools\miniconda3 (
 REM   rmdir /s /q c:\tools\miniconda3
 REM )
-easy_install 
-exit 1
+
 choco install nodejs --yes --version 12.10.0
 choco install microsoft-visual-cpp-build-tools --version 14.0.25420.1 --yes
 choco install git --yes
 REM choco install windows-sdk-10.0 --yes
 
-
-dir C:\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\
-REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\
 REM dir C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\SDK\ScopeCppSDK\SDK\
 
 
-set OLDPWD=%cd%
-REM cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cd %VCINSTALLDIR%
-call for /R %f in (*stdint.h) do set CL=-FI"%f"
-call pip install pycrypto
-IF %ERRORLEVEL% NEQ 0 (
-  exit 1
-)
-cd %OLDPWD%
+REM set OLDPWD=%cd%
+REM REM cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+REM cd %VCINSTALLDIR%
+REM call for /R %f in (*stdint.h) do set CL=-FI"%f"
+REM call pip install pycrypto
+REM IF %ERRORLEVEL% NEQ 0 (
+REM   exit 1
+REM )
+REM cd %OLDPWD%
 
 SET PATH=%PATH%;C:\hostedtoolcache\windows\Python\3.6.8\x64\Scripts
 
