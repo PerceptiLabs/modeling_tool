206a50be2f095cc83d72ec84070a788d63d586d5
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-04 14:06:44 +0100

    sdf

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -14,132 +14,132 @@ mkdir backend_out
 mkdir frontend_out 
 
 cd backend_tmp
-call SET destfolder="..\..\backend"
 
-call FOR /F %a IN (../../scripts/included_files.txt) DO echo F|xcopy /e/h/y /z/i /k /f "%destfolder%/%a" "%a"
+call SET fromfolder=..\..\backend
+call FOR /F %a IN (../../scripts/included_files.txt) DO echo F|xcopy /e/h/y /z/i /k /f "%fromfolder%/%a" "%a"
 
 dir
 
 exit
-REM xcopy /s ..\..\backend . 
-REM exit
-REM REM del minicodehq.py
-REM REM del appOc.py
-REM REM del a2cagent.py
-REM REM del frontend_data_code.py
-REM REM del core_test.py
-REM REM del serverInterface.py
-REM REM del lwInterface.py
 
-REM move setup.py setup.pyx
-REM copy /Y setup.pyx code_generator
-REM copy /Y setup.pyx core_new
-REM copy /Y setup.pyx core_new/data
-REM copy /Y setup.pyx analytics
+xcopy /s ..\..\backend . 
+del minicodehq.py
+del appOc.py
+del a2cagent.py
+del frontend_data_code.py
+del core_test.py
+del serverInterface.py
+del lwInterface.py
 
-REM cd code_generator
-REM mkdir code_generator
-REM move __init__.py __init__.pyx
-REM python setup.pyx develop
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
-REM mv code_generator/* .
-REM rm -rf code_generator
-REM del *.c
-REM del *.py
-REM ren __init__.pyx __init__.py
-REM del setup.pyx
-REM dir
+move setup.py setup.pyx
+copy /Y setup.pyx code_generator
+copy /Y setup.pyx core_new
+copy /Y setup.pyx core_new/data
+copy /Y setup.pyx analytics
 
-REM cd ../core_new
-REM python setup.pyx develop
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
-REM del *.c
-REM del *.py
-REM del setup.pyx
-REM dir
+cd code_generator
+mkdir code_generator
+move __init__.py __init__.pyx
+python setup.pyx develop
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
+mv code_generator/* .
+rm -rf code_generator
+del *.c
+del *.py
+ren __init__.pyx __init__.py
+del setup.pyx
+dir
 
-REM cd data
-REM cp ../../setup.pyx .
-REM dir
-REM mkdir data
-REM move __init__.py __init__.pyx
-REM python setup.pyx develop
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
-REM mv data/* .
-REM rm -rf data
-REM del *.c
-REM del *.py
-REM ren __init__.pyx __init__.py
-REM del setup.pyx
-REM dir
+cd ../core_new
+python setup.pyx develop
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
+del *.c
+del *.py
+del setup.pyx
+dir
 
-REM cd ../../analytics
-REM python setup.pyx develop
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
-REM del *.c
-REM del *.py
-REM del setup.pyx
-REM dir
+cd data
+cp ../../setup.pyx .
+dir
+mkdir data
+move __init__.py __init__.pyx
+python setup.pyx develop
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
+mv data/* .
+rm -rf data
+del *.c
+del *.py
+ren __init__.pyx __init__.py
+del setup.pyx
+dir
 
-REM cd ..
-REM move mainServer.py mainServer.pyx
-REM python setup.pyx develop
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
-REM del *.c
-REM del *.py
-REM del setup.pyx
-REM move mainServer.pyx mainServer.py
+cd ../../analytics
+python setup.pyx develop
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
+del *.c
+del *.py
+del setup.pyx
+dir
+
+cd ..
+move mainServer.py mainServer.pyx
+python setup.pyx develop
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
+del *.c
+del *.py
+del setup.pyx
+move mainServer.pyx mainServer.py
 
 
 
-REM copy ..\..\backend\windows.spec .
-REM pyinstaller --clean -y windows.spec
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   dir
-REM   exit 1
-REM )
-REM call "C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 "dist/appServer/*.exe"
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   dir
-REM   exit 1
-REM )
+copy ..\..\backend\windows.spec .
+pyinstaller --clean -y windows.spec
+IF %ERRORLEVEL% NEQ 0 (
+  dir
+  exit 1
+)
+call "C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 "dist/appServer/*.exe"
+IF %ERRORLEVEL% NEQ 0 (
+  dir
+  exit 1
+)
 
-REM cd ..\backend_out
-REM mkdir dist
-REM xcopy ..\backend_tmp\dist dist\ /sy 
+cd ..\backend_out
+mkdir dist
+xcopy ..\backend_tmp\dist dist\ /sy 
 
-REM cd ..\..\frontend\
+cd ..\..\frontend\
 
-REM rmdir /s /q core
-REM mkdir core
+rmdir /s /q core
+mkdir core
 
-REM xcopy ..\build\backend_out\dist\appServer core\ /sy 
-REM dir core
+xcopy ..\build\backend_out\dist\appServer core\ /sy 
+dir core
 
-REM call npm run build
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   dir build
-REM   exit 1
-REM )
-REM dir build
+call npm run build
+IF %ERRORLEVEL% NEQ 0 (
+  dir build
+  exit 1
+)
+dir build
 
-REM call "C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 "build/*.exe"
-REM IF %ERRORLEVEL% NEQ 0 (
-REM   exit 1
-REM )
+call "C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 "build/*.exe"
+IF %ERRORLEVEL% NEQ 0 (
+  exit 1
+)
 
-REM copy build\*.exe ..\build\frontend_out\ 
+copy build\*.exe ..\build\frontend_out\ 
 
-REM cd ..\scripts
+cd ..\scripts
 
 
