28b5df3a17af45dd74e4ce6a9ec92205cb32b452
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-09 16:48:28 +0100

    test

--- scripts/test_windows.bat
+++ scripts/test_windows.bat
@@ -1,7 +1,9 @@
+echo "Installing dependencies"
 call python -m pip install --upgrade pip setuptools
 call pip install -r requirements.txt
 call pip install dask[array] --upgrade
 
 cd backend
 
+echo "Running tests"
 python python_error_checks.py
