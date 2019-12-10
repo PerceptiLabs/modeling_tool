44ab39894f6d45d1ba7434b6826d19d9fcce2fd7
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-10 11:10:39 +0100

    fixing some bugs in unused files

--- scripts/test_windows.bat
+++ scripts/test_windows.bat
@@ -2,7 +2,7 @@ echo "Installing dependencies"
 call python -m pip install --upgrade pip setuptools
 call pip install -r requirements.txt
 call pip install dask[array] --upgrade
-call pip install pylint
+call pip install pylint==2.4.3
 
 cd ../backend
 del *.c
