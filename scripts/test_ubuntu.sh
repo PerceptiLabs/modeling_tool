ed81743831dcf82bcf859cc87c628fd8e56fcafd
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-10 15:13:20 +0100

    sdf

--- scripts/test_ubuntu.sh
+++ scripts/test_ubuntu.sh
@@ -33,3 +33,5 @@ echo "Running tests"
 python python_error_checks.py
 if [ $? -eq 2 ]; then exit 1; fi
 if [ $? -eq 1 ]; then exit 0; fi
+
+python -m pytest
--- scripts/test_windows.bat
+++ scripts/test_windows.bat
@@ -3,6 +3,7 @@ call python -m pip install --upgrade pip setuptools
 call pip install -r requirements.txt
 call pip install dask[array] --upgrade
 call pip install pylint==2.4.3
+call pip install pytest==5.3.1
 
 cd ../backend
 del *.c
@@ -14,6 +15,8 @@ IF %ERRORLEVEL% EQU 2 (
 IF %ERRORLEVEL% EQU 1 (
   exit 0
 )
+
+python -m pytest
 
 
 REM FOR /F %%a IN (../backend/included_files.txt) DO (
