866342c0dc321e34a0acfbd22dfee7664cb2a116
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-10 12:56:28 +0100

    sdf

--- scripts/test_windows.bat
+++ scripts/test_windows.bat
@@ -9,7 +9,11 @@ del *.c
 echo "Running tests"
 python python_error_checks.py
 IF %ERRORLEVEL% EQU 2 (
+  echo "Exiting from python error checks"
   exit 1
+)
+IF %ERRORLEVEL% EQU 1 (
+  exit 0
 )
 
 REM FOR /F %%a IN (../scripts/included_files.txt) DO cython %%a 
