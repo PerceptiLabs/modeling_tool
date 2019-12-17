e81f64dcd92129756c8c4a500a77bba5f7953923
Nikita V. Pastukhov <nikita.pastukhov@ssa.group>
2019-12-17 15:45:19 +0200

    changed id to name for core sending

--- frontend/src/renderer/store/modules/mod_api.js
+++ frontend/src/renderer/store/modules/mod_api.js
@@ -28,6 +28,15 @@ const getters = {
         const filePath = el.checkpoint[1].slice(0, el.checkpoint[1].length);
         checkpointPath[1] = rootPath + pathSlash + filePath;
       }
+
+      let namesConnectionOut = el.connectionOut.map((currentValue)=> {
+        return  network.networkElementList[currentValue].layerName
+      });
+
+      let namesConnectionIn = el.connectionIn.map((currentValue)=> {
+        return  network.networkElementList[currentValue].layerName
+      });
+
       /*prepare elements*/
       if(dataLayers.includes(el.componentName)) {
         layers[el.layerId] = {
@@ -37,9 +46,11 @@ const getters = {
           checkpoint: checkpointPath,
           endPoints: el.endPoints,
           //Code: el.coreCode,
-          backward_connections: el.connectionIn,
-          forward_connections: el.connectionOut
+          backward_connections: namesConnectionIn,
+          forward_connections: namesConnectionOut
         };
+        //console.log('1backward_connections', namesConnectionIn);
+        //console.log('1forward_connections', namesConnectionOut);
       }
       else {
         layers[el.layerId] = {
@@ -49,10 +60,12 @@ const getters = {
           endPoints: el.endPoints,
           Properties: el.layerSettings,
           Code: el.layerCode,
-          backward_connections: el.connectionIn,
-          forward_connections: el.connectionOut
+          backward_connections: namesConnectionIn,
+          forward_connections: namesConnectionOut
         };
       }
+      //console.log('2backward_connections', namesConnectionIn);
+      //console.log('2forward_connections', namesConnectionOut);
     }
     return layers
   }
