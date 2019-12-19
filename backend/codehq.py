f4b75208172547a931fcef255c56098618f72fae
Anton Kollmats <antonkollmats2@gmail.com>
2019-12-19 13:16:19 -0500

    changed directory structure. new style code generators now have their own directory. legacy reuses classes from there

--- backend/codehq.py
+++ backend/codehq.py
@@ -5,7 +5,8 @@ import logging
 from code_generator import CustomCodeGenerator, CodePart
 from code_generator.datadata import DataDataCodeGenerator
 from code_generator.dataenv import DataEnvironmentCodeGenerator
-from code_generator.generators import DataDataCodeGenerator2
+
+from code.generators import DataDataCodeGenerator2
 
 from code_generator.tensorflow import FullyConnectedCodeGenerator, ConvCodeGenerator, DeconvCodeGenerator, RecurrentCodeGenerator, CropCodeGenerator, WordEmbeddingCodeGenerator, GrayscaleCodeGenerator, OneHotCodeGenerator, ReshapeCodeGenerator, ArgmaxCodeGenerator, MergeCodeGenerator, SoftmaxCodeGenerator, TrainNormalCodeGenerator, TrainLossCodeGenerator, TrainOptimizerCodeGenerator, TrainReinforceCodeGenerator, LayerPair
 
