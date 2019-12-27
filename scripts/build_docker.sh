b746df549e30457a854b8b8d33aa8b643ec601e7
Robert Lundberg <Lundbergrobert@telia.com>
2019-12-27 10:53:27 +0100

    base for docker CI

---
+++ scripts/build_docker.sh
@@ -0,0 +1,58 @@
+################### BUILD CORE BINARIES #######################
+echo "Activating conda environment"
+conda activate py362_
+
+echo "Python location:"
+which python3
+
+
+echo "Conda list:"
+conda list
+
+# ----- Build backend ----
+echo "----- Building backend -----"
+cd ..
+rm -rf build
+mkdir build
+cd build/
+
+echo "Cleaning up build directory"
+rm -rf backend_out
+rm -rf frontend_out
+
+mkdir backend_out
+mkdir frontend_out
+
+echo "Copying files files from ../../backend/"
+cd backend_out/
+rsync -a ../../backend --files-from=../../backend/included_files.txt .
+cp ../../backend/setup_compact.pyx .
+
+echo "C compiling"
+mv mainServer.py mainServer.pyx
+find . -name "__init__.py" -exec rename -v 's/\.py$/\.pyx/i' {} \;
+python setup_compact.pyx develop --user
+if [ $? -ne 0 ]; then exit 1; fi
+
+echo "Cleaning up after the compilation"
+find . -type f -name '*.c' -exec rm {} +
+find . -type f -name '*.py' -exec rm {} +
+rm setup_compact.pyx
+rm -r build
+mv mainServer.pyx mainServer.py
+find . -name "__init__.pyx" -exec rename -v 's/\.pyx$/\.py/i' {} \;
+
+echo "Listing files to be included in build (contents of 'backend_out/')"
+ls -l
+
+################### BUILD FRONTEND #######################
+echo "----- Building frontend -----"
+cd ../../frontend/src
+npm run build
+
+cp dist/* ../build/frontend_out/
+
+################### MOVING EVERYTHING TO CORRECT PLACES #######################
+cd ../../
+cp Docker/Fronend/* build/frontend_out
+cp Docker/Core/* build/backend_out
