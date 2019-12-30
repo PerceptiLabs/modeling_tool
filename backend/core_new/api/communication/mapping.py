16e410c30ab800417b72d7be4ba186a765752609
Anton Kollmats <antonkollmats2@gmail.com>
2019-12-30 18:19:33 -0500

    small refactor

--- backend/core_new/api/communication/mapping.py
+++ backend/core_new/api/communication/mapping.py
@@ -3,66 +3,51 @@ import threading
 import struct
 import queue
 import zmq
+from abc import ABC, abstractmethod
 from zmq.eventloop.ioloop import IOLoop, PeriodicCallback
 from zmq.eventloop.zmqstream import ZMQStream
+
 
 def int2bytes(x):
     return struct.pack('!q', x)
 
+
 def bytes2int(x):
     return struct.unpack('!q', x)[0]    
 
-'''
-class MappedMessage:
-    def __init__(self, counter: int, key: bytes, body: bytes):
-        assert isinstance(key, bytes)
-        assert isinstance(body, bytes) or body is None
-        assert isinstance(counter, int) 
-        
-        self._key = key
-        self._body = body
-        self._counter = counter
-
-    @classmethod
-    def recv(cls, socket, counter=None):
-        raw_message = socket.recv_multipart()
-        return cls.from_raw_message(raw_message, counter)
-
-    @classmethod
-    def from_raw_message(cls, raw_message, counter=None):
-        key, b_counter, body = raw_message
-        counter = counter if counter is not None else struct.unpack('!q', b_counter)[0]
-        return cls(counter, key, body)
-
-    def send(self, socket):
-        key = self._key or b''
-        body = self._body or b''
-        counter = self._counter
-        b_counter = struct.pack('!q', counter)
-        socket.send_multipart([key, b_counter, body])
-
-    @property
-    def counter(self):
-        return self._counter
-
-    @property
-    def key(self):
-        return self._key
-    
-    @property
-    def body(self):
-        return self._body
-
-    def __repr__(self):
-        return f"key: {self._key}, seq: {self._counter}, body: {self._body}"
-'''
 
 MappedMessage = collections.namedtuple(
     'MappedMessage',
     ['index', 'key', 'body']
 )
 
-class MapServer:
+
+class MapBase(ABC):
+    def _reset(self):
+        self._worker = None        
+        self._lock = threading.RLock()
+        self._messages = {}
+        self._index = 0
+        self._is_running = threading.Event()
+        self._is_running.clear()
+
+    @abstractmethod
+    def _worker_func(self):
+        raise NotImplementedError
+                
+    def start(self):
+        self._is_running.set()        
+        self._worker = threading.Thread(target=self._worker_func, daemon=True)
+        self._worker.start()
+
+    def stop(self):
+        if self._is_running.is_set():
+            self._is_running.clear()                        
+            self._worker.join()
+        self._reset()
+    
+
+class MapServer(MapBase):
     POLL_INTERVAL = 1
     
     def __init__(self, router_addr, pub_addr, pull_addr):
@@ -70,14 +55,6 @@ class MapServer:
         self._router_addr = router_addr
         self._pub_addr = pub_addr
         self._pull_addr = pull_addr
-        
-    def _reset(self):
-        self._lock = threading.RLock()        
-        self._queue = None        
-        self._worker = None        
-        self._stop_event = None 
-        self._update_counter = 0
-        self._messages = {}
         
     def _worker_func(self):
         ctx = zmq.Context()        
@@ -93,7 +70,7 @@ class MapServer:
         poller.register(snapshot_socket, zmq.POLLIN)
         poller.register(collector_socket, zmq.POLLIN)        
 
-        while not self._stop_event.is_set():
+        while self._is_running.is_set():
             items = dict(poller.poll(timeout=self.POLL_INTERVAL))
             
             if snapshot_socket in items:
@@ -116,29 +93,17 @@ class MapServer:
             snapshot_socket.send_multipart([identity, b'snapshot-end', int2bytes(0), b'', b''])
             
     def _on_update(self, collector_socket, publisher_socket):
-        descr, _, key, body = collector_socket.recv_multipart()
-        assert descr in [b'update-set']
+        op, _, key, body = collector_socket.recv_multipart()
+        assert op.decode('utf-8') in ['update-set']
         
         with self._lock:
-            index = self._update_counter
-            publisher_socket.send_multipart([descr, int2bytes(index), key, body])
+            index = self._index
+            publisher_socket.send_multipart([op, int2bytes(index), key, body])
             self._messages[key] = MappedMessage(index, key, body)
-            self._update_counter += 1
+            self._index += 1
+
             
-    def start(self):
-        self._stop_event = threading.Event()        
-        
-        self._worker = threading.Thread(target=self._worker_func, daemon=True)
-        self._worker.start()
-
-    def stop(self):
-        if self._stop_event is not None:
-            self._stop_event.set()
-            self._worker.join()
-        self._reset()
-
-        
-class MapClient:
+class MapClient(MapBase):
     POLL_INTERVAL = 1 # [ms]
     
     def __init__(self, dealer_addr, sub_addr, push_addr):
@@ -147,14 +112,6 @@ class MapClient:
         self._sub_addr = sub_addr
         self._push_addr = push_addr
         
-    def _reset(self):
-        self._lock = threading.RLock()                
-        self._queue = queue.Queue()        
-        self._worker = None        
-        self._stop_event = None 
-        self._index = 0
-        self._messages = {}
-
     def _worker_func(self):
         ctx = zmq.Context()
         snapshot_socket = self._init_socket(ctx, zmq.DEALER, self._dealer_addr)
@@ -163,65 +120,54 @@ class MapClient:
 
         self._get_snapshot(snapshot_socket)
         
-        if self._stop_event.is_set():
+        if not self._is_running.is_set():
             return
 
-        self._get_updates(subscriber_socket, publisher_socket)
+        self._get_and_send_updates(subscriber_socket, publisher_socket)
 
-    def _get_updates(self, subscriber_socket, publisher_socket):
+    def _get_and_send_updates(self, subscriber_socket, publisher_socket):
         poller = zmq.Poller()
         poller.register(subscriber_socket, zmq.POLLIN)
         
-        while not self._stop_event.is_set():
+        while self._is_running.is_set():
             items = dict(poller.poll(timeout=self.POLL_INTERVAL))
 
             if subscriber_socket in items:
-                descr, index, key, body = subscriber_socket.recv_multipart()
+                op, index, key, body = subscriber_socket.recv_multipart()
                 index = bytes2int(index)
                 
                 with self._lock:
                     if self._index != index:
-                        raise RuntimeError(f"Unexpected index. Expected {self._index + 1} but got {index}!")
+                        raise RuntimeError(f"Unexpected index. Expected {self._index} but got {index}!")
                     self._messages[key] = MappedMessage(index, key, body)                
                     self._index += 1
                     
-            if self._queue is not None and self._queue.qsize() > 0:
-                descr, key, body = self._queue.get()
-                assert descr in [b'update-set']
-                publisher_socket.send_multipart([descr, int2bytes(-1), key, body])
+            if self._queue.qsize() > 0:
+                op, key, body = self._queue.get()
+                assert op in ['update-set']
+                publisher_socket.send_multipart([op.encode(), int2bytes(-1), key, body])
 
     def _get_snapshot(self, snapshot_socket):
         snapshot_socket.send_string('snapshot-get')
         
         new_messages = []
-        while not self._stop_event.is_set():
-            descr, index, key, body = snapshot_socket.recv_multipart()
+        while self._is_running.is_set():
+            op, index, key, body = snapshot_socket.recv_multipart()
+            op = op.decode('utf-8')            
             index = bytes2int(index)
             
-            if descr == b'snapshot-set':
+            if op == 'snapshot-set':
                 message = MappedMessage(index, key, body)
                 new_messages.append(message)
-            elif descr == b'snapshot-end':
+            elif op == 'snapshot-end':
                 break
             else:
-                raise ValueError(f"Unknown descriptor {descr} received over snapshot socket")
+                raise ValueError(f"Unknown operation '{op}' received over snapshot socket")
             
         with self._lock:
             for message in new_messages:
                 self._messages[message.key] = message
                 self._index = max(self._index, message.index)
-
-                
-    def start(self):
-        self._stop_event = threading.Event()                        
-        self._worker = threading.Thread(target=self._worker_func, daemon=True)
-        self._worker.start()
-
-    def stop(self):
-        if self._stop_event is not None:
-            self._stop_event.set()
-            self._worker.join()
-        self._reset()
 
     def _init_socket(self, ctx, zmq_type, address, opts=None):
         socket = ctx.socket(zmq_type)
@@ -232,7 +178,11 @@ class MapClient:
             socket.setsockopt_string(opt[0], opt[1])
 
         socket.connect(address)
-        return socket    
+        return socket
+
+    def _reset(self):
+        super()._reset()
+        self._queue = queue.Queue()
                 
     @property
     def messages(self):
@@ -240,12 +190,18 @@ class MapClient:
             messages = self._messages.copy()
         return messages
 
-    @property
-    def queue(self):
-        return self._queue
 
+    def put(self, operation: str, key: bytes, value: bytes):
+        if not isinstance(operation, str):
+            raise TypeError("Operation must be of type 'str'")
+        if not isinstance(key, bytes):
+            raise TypeError("Key must be of type 'bytes'")
+        if not isinstance(value, bytes):
+            raise TypeError("Value must be of type 'bytes'")
 
-class ByteMapping(collections.MutableMapping):
+        self._queue.put((operation, key, value))
+
+class ByteMap(collections.MutableMapping):
     def __init__(self, client):
         self._client = client
     
--- backend/core_new/api/communication/test_mapping.py
+++ backend/core_new/api/communication/test_mapping.py
@@ -26,13 +26,22 @@ def client():
     yield client
     client.stop()
 
+@pytest.fixture(autouse=True)
+def client2():
+    client = MapClient(
+        'tcp://localhost:5556',
+        'tcp://localhost:5557',
+        'tcp://localhost:5558'
+    )    
+    yield client
+    client.stop()
+
     
 def test_client_server_queued_message_is_stored(server, client):
-    server.start()
-    client.start()
+    server.start(); client.start()
     
-    client.queue.put((b'update-set', b'key', b'value'))
-    time.sleep(0.5)
+    client.put('update-set', b'key', b'value')
+    time.sleep(0.3)
     
     assert (b'key') in client.messages
     assert client.messages[b'key'].key == b'key'
@@ -40,11 +49,10 @@ def test_client_server_queued_message_is_stored(server, client):
 
     
 def test_client_server_stopped_client_is_empty(server, client):
-    server.start()
-    client.start()
+    server.start(); client.start()
     
-    client.queue.put((b'update-set', b'key', b'value'))
-    time.sleep(0.5)
+    client.put('update-set', b'key', b'value')
+    time.sleep(0.3)
     assert client.messages != {}
     
     client.stop()
@@ -52,16 +60,31 @@ def test_client_server_stopped_client_is_empty(server, client):
 
     
 def test_client_server_restarted_client_catches_up(server, client):
-    server.start()
-    client.start()
+    server.start(); client.start()
     
-    client.queue.put((b'update-set', b'key', b'value'))
-    time.sleep(0.5)
+    client.put('update-set', b'key', b'value')
+    time.sleep(0.3)
     assert client.messages != {}    
     
     client.stop()
     assert client.messages == {}
     
     client.start()
-    time.sleep(0.5)
+    time.sleep(0.3)
     assert client.messages != {}
+
+
+def test_second_client_receives_message(server, client, client2):
+    server.start(); client.start(); client2.start()
+
+    client.put('update-set', b'key_1', b'value_1')
+    client2.put('update-set', b'key_2', b'value_2')
+    time.sleep(0.3)    
+
+    assert (b'key_1') in client.messages
+    assert (b'key_2') in client.messages    
+    assert (b'key_1') in client2.messages
+    assert (b'key_2') in client2.messages    
+    
+    
+    
