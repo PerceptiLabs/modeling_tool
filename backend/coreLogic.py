57dbfa89a48bde8036713177a46b584c020c315f
Robert Lundberg <Lundbergrobert@telia.com>
2020-01-17 17:47:51 +0100

    fixed visualizations

--- backend/coreLogic.py
+++ backend/coreLogic.py
@@ -277,9 +277,7 @@ class coreLogic():
     def get_gpu(self):
         gpus = GPUtil.getGPUs()
         loadList = [gpu.load*100 for gpu in gpus]
-        nameList = [gpu.name for gpu in gpus]
-        for i in range(len(nameList)):
-            print(str(nameList[i]) + " : " + str(loadList[i]) + "%")
+        
         if loadList:
             return np.average(loadList)
         else:
