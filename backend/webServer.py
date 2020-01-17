0451a7a39f0614196b4eb09e70191da94c56c168
Robert Lundberg <Lundbergrobert@telia.com>
2020-01-17 11:04:27 +0100

    made sure we always get columns for pandas

--- backend/webServer.py
+++ backend/webServer.py
@@ -637,6 +637,29 @@ class Message:
         sys.exit(1)
 
 
+def setup_logger(log_level):
+    """ Sets up logging for the application.
+
+    In other modules, simply call log = logging.getLogger(__name__) after importing logging. 
+    In production, set the logging level to something higher (e.g., ERROR). 
+
+    Levels:
+        DEBUG: Detailed information, for diagnosing problems. 
+        INFO: Confirm things are working as expected. 
+        WARNING: Something unexpected happened, or indicative of some problem. But the software is still working as expected. 
+        ERROR: More serious problem, the software is not able to perform some function. 
+        CRITICAL: A serious error, the program itself may be unable to continue running. 
+    """
+    FORMAT = '%(asctime)s - %(levelname)s - %(threadName)s - %(filename)s:%(lineno)d - %(message)s'
+    FILE_NAME = 'backend.log'
+    
+    logging.basicConfig(stream=sys.stdout,
+                        format='%(asctime)s - %(levelname)s - %(threadName)s - %(filename)s:%(lineno)d - %(message)s',
+                        level=logging.getLevelName(log_level))
+
+
+setup_logger("INFO")
+
 cores=dict()
 dataDict=dict()
 checkpointDict=dict()
