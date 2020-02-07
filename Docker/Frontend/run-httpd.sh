632541914742fe696ece38b7102edfe86ea77737
David Han <david_han@hotmail.com>
2020-02-07 10:03:47 -0800

    FIXES where the Node server was not runnning in the background

--- Docker/Frontend/run-httpd.sh
+++ Docker/Frontend/run-httpd.sh
@@ -5,5 +5,5 @@
 # if it thinks it is already running. 
 rm -rf /run/httpd/* /tmp/httpd*
 
-exec node envServer.js
-exec /usr/sbin/apachectl -D FOREGROUND
+exec node envServer.js &
+exec /usr/sbin/httpd -D FOREGROUND
