8c23cbda7cce0cd5838ecacd51b18a23374de63e
Robert Lundberg <Lundbergrobert@telia.com>
2020-02-07 09:57:09 +0100

    renamed for clarity

--- scripts/tests_setup_ubuntu.sh
+++ scripts/tests_setup_ubuntu.sh
@@ -1,5 +1,5 @@
 CONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-CONDA_ENV_FILE="../backend/environment.yml"
+CONDA_ENV_FILE="../backend/environment_linux-py36.yml"
 
 # TODO: npm, gcc and conda versions should be explicitly declared to ensure reproducibility.
 
