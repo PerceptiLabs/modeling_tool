4e3e83f144df169b4d46b2964f436eaebe94b205
David Han <david_han@hotmail.com>
2020-02-27 15:14:36 -0800

    ADDS styling to the popup

--- frontend/src/renderer/components/global-popups/file-picker-popup.vue
+++ frontend/src/renderer/components/global-popups/file-picker-popup.vue
@@ -1,38 +1,38 @@
 <template lang="pug">
-    base-global-popup(:tab-set="popupTitle")
-        template(slot=slotContentName)
+  base-global-popup(:tab-set="popupTitle")
+    template(:slot="slotContentName")
 
-            .filepicker
-                .directory-breadcrumb
-                    .breadcrumb(
-                        @click="calcBreadcrumbPath(pIdx)"
-                        v-for="(p, pIdx) in currentPath" 
-                        :key="p") 
-                        span {{ p }}
+      .filepicker
+        .directory-breadcrumb
+          .breadcrumb(
+            @click="calcBreadcrumbPath(pIdx)"
+            v-for="(p, pIdx) in currentPath" 
+            :key="p") 
+            span {{ p }}
 
-                .selectable-list
-                    .list-item(
-                        @click="calcFolderPath(dIdx)"
-                        v-for="(d,dIdx) in directories" 
-                        :key="d") 
-                        img(src="/static/file-picker/folder.svg") 
-                        span {{ d }}
+        .selectable-list
+          .list-item(
+            @click="calcFolderPath(dIdx)"
+            v-for="(d,dIdx) in directories" 
+            :key="d") 
+            img(src="/static/file-picker/folder.svg") 
+            span {{ d }}
 
-                    .list-item(
-                        :class="{selected:isSelected(f)}"
-                        @click="toggleSelectedFile(f)"
-                        v-for="f in files" 
-                        :key="f")
-                        img(src="/static/file-picker/file.svg")
-                        span {{ f }}
+          .list-item(
+            :class="{selected:isSelected(f)}"
+            @click="toggleSelectedFile(f)"
+            v-for="f in files" 
+            :key="f")
+            img(src="/static/file-picker/file.svg")
+            span {{ f }}
 
-        template(slot="action")
-            button.btn.btn--primary.btn--disabled(type="button"
-                @click="closePopup"
-                ) Cancel
-            button.btn.btn--primary(type="button"
-                @click="applySet"
-                ) Confirm
+    template(slot="action")
+      button.btn.btn--primary.btn--disabled(type="button"
+        @click="closePopup"
+        ) Cancel
+      button.btn.btn--primary(type="button"
+        @click="applySet"
+        ) Confirm
 
 </template>
 
@@ -42,127 +42,129 @@ import BaseGlobalPopup  from '@/components/global-popups/base-global-popup';
 import { coreRequest } from '@/core/apiWeb.js';
 
 export default {
-    name: 'FilePicker',
-    components: {BaseGlobalPopup},
-    data() {
-        return {
-            popupTitle: ['Load files'],
-            currentPath: [],
-            directories: [],
-            files: [],
-            selectedFiles: []
-        }
-    },
-    methods: {
-        isSelected(name) {
-            return (this.selectedFiles.includes(name));
-        },
-        calcBreadcrumbPath(pathIdx) {
-            let breadcrumbPath = '/' + this.currentPath.slice(0,pathIdx + 1).join('/');
-            this.fetchPathInformation(breadcrumbPath);
-        },
-        calcFolderPath(folderIndex) {
-            let folderPath = '/' + this.currentPath.join('/') + '/' + this.directories[folderIndex];
-            console.log(folderPath);
-            this.fetchPathInformation(folderPath);
-        },
-        toggleSelectedFile(fileName) {
-            if (this.selectedFiles.includes(fileName)) {
-                const idxToRemove = this.selectedFiles.findIndex(el => el === fileName);
-                this.selectedFiles.splice(idxToRemove, 1);
-            } else {
-                this.selectedFiles.push(fileName);
-            }
-        },
-        fetchPathInformation(path) {
-            this.selectedFiles = [];
-
-            let theData = {
-                reciever: '0',
-                action: 'getFolderContent',
-                value: path
-            };
-
-            console.log('request', theData);
-
-            coreRequest(theData)
-            .then(jsonData => {
-                console.log('response', jsonData);
-                this.currentPath = jsonData.current_path.split('/').filter(el => el);   
-                this.directories = jsonData.dirs;
-                this.files = jsonData.files;
-            })
-        },
-        applySet() {
-            this.closePopup();
-        },
-        closePopup() {
-            this.$store.commit('globalView/HIDE_allGlobalPopups');
-        },
-    },
-    computed: {
-        slotContentName() {
-            return this.popupTitle.first() + '-content';
-        }
-    },
-    mounted() {
-        this.fetchPathInformation('');
+  name: 'FilePicker',
+  components: {BaseGlobalPopup},
+  data() {
+    return {
+      popupTitle: ['Load files'],
+      currentPath: [],
+      directories: [],
+      files: [],
+      selectedFiles: []
     }
+  },
+  methods: {
+    isSelected(name) {
+      return (this.selectedFiles.includes(name));
+    },
+    calcBreadcrumbPath(pathIdx) {
+      let breadcrumbPath = '/' + this.currentPath.slice(0,pathIdx + 1).join('/');
+      this.fetchPathInformation(breadcrumbPath);
+    },
+    calcFolderPath(folderIndex) {
+      let folderPath = '/' + this.currentPath.join('/') + '/' + this.directories[folderIndex];
+      console.log(folderPath);
+      this.fetchPathInformation(folderPath);
+    },
+    toggleSelectedFile(fileName) {
+      if (this.selectedFiles.includes(fileName)) {
+          const idxToRemove = this.selectedFiles.findIndex(el => el === fileName);
+          this.selectedFiles.splice(idxToRemove, 1);
+      } else {
+          this.selectedFiles.push(fileName);
+      }
+    },
+    fetchPathInformation(path) {
+      this.selectedFiles = [];
+
+      let theData = {
+          reciever: '0',
+          action: 'getFolderContent',
+          value: path
+      };
+
+      console.log('request', theData);
+
+      coreRequest(theData)
+      .then(jsonData => {
+          console.log('response', jsonData);
+          this.currentPath = jsonData.current_path.split('/').filter(el => el);   
+          this.directories = jsonData.dirs;
+          this.files = jsonData.files;
+      });
+    },
+    applySet() {
+      this.closePopup();
+    },
+    closePopup() {
+      this.$store.commit('globalView/HIDE_allGlobalPopups');
+    },
+  },
+  computed: {
+    slotContentName() {
+      return this.popupTitle ? this.popupTitle[0] + '-content' : '';
+    }
+  },
+  mounted() {
+    this.fetchPathInformation('');
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
+@import "../../scss/base";
+@import "../../scss/common/info-popup";
 
 .filepicker {
-  background-color: white;
+//   background-color: $bg-window;
   height: 24rem;
   width: 48rem;
-  padding: 2rem;
+  margin: 2rem;
   overflow-y: auto;
+  font-size: 1.1rem;
 }
 
 .directory-breadcrumb {
-    display: flex;
-    margin: 1rem;
+  display: flex;
+  margin-bottom: 1rem;
 
-    .breadcrumb {
-        cursor: pointer;
+  .breadcrumb {
+    cursor: pointer;
 
-        & + .breadcrumb:before
-        {
-            content: '\00a0\00a0>\00a0\00a0';
-        }
+    & + .breadcrumb:before
+    {
+      content: '\00a0\00a0>\00a0\00a0';
     }
+  }
 }
 
 .selectable-list {
 
-    .list-item { 
-        display: flex;
-        justify-content: left;
-        align-items: center;
-        padding: 0 1rem;
-        // width: 100%;
+  .list-item { 
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    padding: 0 1rem;
 
-        img {
-            margin-right: 1rem;
-        }
-
-        &:hover {
-            background-color: powderblue;
-        }
-
-        &.selected {
-            background-color: powderblue;
-        }
+    img {
+      margin-right: 1rem;
     }
-    
+
+    &:hover {
+      background-color: powderblue;
+    }
+
+    &.selected {
+      background-color: powderblue;
+    }
+  }
+  
 }
 
 img {
-    width: 1rem;
-    height: 1rem;
+  width: 1rem;
+  height: 1rem;
 }
 
 </style>
