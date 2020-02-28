1e16e4eadac38663eab5fcdadcb5cb80d0b9fe33
Anton Bourosu <anton@smartdata.solutions>
2020-02-28 17:58:44 +0200

    Remove / from end of file

--- frontend/src/renderer/components/different/file-picker.vue
+++ frontend/src/renderer/components/different/file-picker.vue
@@ -101,7 +101,7 @@ export default {
         console.log('Clicked OK', this.selectedFiles);
         this.$emit(
           'files-selected',
-          this.selectedFiles.map(f => this.osPathPrefix + this.currentPath.join('/') + '/' + f + this.osPathSuffix));
+          this.selectedFiles.map(f => this.osPathPrefix + this.currentPath.join('/') + '/' + f));
     },
     onCancel() {
         this.$emit('close');
