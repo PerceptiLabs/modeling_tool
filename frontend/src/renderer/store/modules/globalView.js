aeeda6d91f352400493594def69d305eb5eb1a69
David Han <david_han@hotmail.com>
2020-02-27 22:17:15 -0800

    UPDATES filepicker to not popup on start

--- frontend/src/renderer/store/modules/globalView.js
+++ frontend/src/renderer/store/modules/globalView.js
@@ -19,7 +19,7 @@ const state = {
     showErrorPopup: false,
     showWorkspaceBeforeImport: false,
     showConfirmPopup: false,
-    showFilePicker: true,
+    showFilePicker: false,
   },
   popupConfirmCancel: null,
   popupConfirmOk: null,
