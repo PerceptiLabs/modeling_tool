b3db3dcb34c9c38fb33e6f226d543161d549b260
Anton Bourosu <anton@smartdata.solutions>
2020-03-18 19:33:19 +0200

    Models, view. Work in progress..

---
+++ frontend/src/renderer/pages/layout/project-sidebar.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+  div.wrapper
+    router-link.logo-link(
+      :to="{name: 'app'}")
+      img(src="../../../../static/img/project-page/logo.svg")
+    router-link.nav-link(
+      :to="{name: 'app'}")
+      img(src="../../../../static/img/project-page/file.svg")
+    router-link.nav-link.is-active(
+      :to="{name: 'app'}")
+      img(src="../../../../static/img/project-page/search.svg")
+    router-link.nav-link(
+      :to="{name: 'app'}")
+      img(src="../../../../static/img/project-page/repository.svg")
+    router-link.nav-link(
+      :to="{name: 'app'}")
+      img(src="../../../../static/img/project-page/debug.svg")
+</template>
+
+<script>
+  export default {
+    name: 'projectSidebar'
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    position: fixed;
+    left: 0;
+    width: 40px;
+    height: 100vh;
+    background: #2E3340;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  .logo-link {
+    display: block;
+    padding-top: 10px;
+    margin-left: 1px;
+    margin-bottom: 5px;
+  }
+  .nav-link {
+    position: relative;
+    display: flex;
+    height: 40px;
+    justify-content: center;
+    align-self: center;
+    &.is-active {
+      &:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-color: #fff;
+        width: 2px;
+        height: 100%;
+      }
+    }
+  }
+</style>
--- frontend/src/renderer/pages/projects/projects.vue
+++ frontend/src/renderer/pages/projects/projects.vue
@@ -1,237 +1,267 @@
 <template lang="pug">
-  .page-projects
-    aside.page-projects_sidebar
-      include ./sidebar/sidebar.pug
-    .page-projects_basic-templates
-      include ./basic-templates/basic-templates.pug
-      router-link.btn.btn--outline-blue-rev.projects-templates_link(
-        v-if="networkIsNotEmpty"
-        :to="{name: 'app'}")
-        span Back to Workspace
-        i.icon.icon-shevron-right
-    main.page-projects_recent-files
-      include ./recent-files/recent-files.pug
+  div
+    project-sidebar
+    div.project-wrapper
+      h3.title.pl-40 /Models
+      div.header-controls
+        div.left-side
+          span.btn-round-icon
+            img(src="../../../../static/img/project-page/minus.svg")
+          span.btn-round-icon
+            img(src="../../../../static/img/project-page/plus.svg")
+          div.search-input
+            img(src="../../../../static/img/project-page/search-input.svg")
+            input(
+              type="text"
+              placeholder="Search"
+              v-bind="searchValue"
+            )
+        div.right-side
+          img.img-button(v-if="isAtLeastOneItemSelected()" @click="toggleFavoriteItems()" src="../../../../static/img/project-page/star.svg")
+          img.img-button(v-if="isAtLeastOneItemSelected()" @click="removeItems()" src="../../../../static/img/project-page/remove.svg")
+          span.text-button(v-if="isAtLeastOneItemSelected()") Open
+          span.text-button(v-if="isAtLeastOneItemSelected()") BlackBox
+          span.text-button(v-if="isAtLeastOneItemSelected()") History
+          span.text-button(v-if="isAtLeastOneItemSelected()" :class="{'is-disable': isDisabledCompareBtn()}") Compare
+          sort-by-button(
+            :options="sortOptions"
+            :optionSelected="isSelectedSortType"
+            @onSelectHandler="onSortByChanged"
+          )
+      div.models-list
+        div.models-list-row.model-list-header
+          div.column-1 Name
+          div.column-2 Status
+          div.column-3 Saved Version
+          div.column-4 Session End Time
+          div.column-5 Collaborators
+          div.column-6 Last Modified
+        div.models-list-row.model-list-item(v-for="model in modelList" :key="model.id" :class="{'is-selected': isItemSelected(model.id)}")
+          div.column-1
+            span.btn-round-icon(@click="toggleItemSelection(model.id)")
+              img(v-if="isItemSelected(model.id)" src="../../../../static/img/project-page/checked.svg")
+            span {{model.name}}
+            svg.is-favorite(v-if="model.isFavorite" width="21" height="19" viewBox="0 0 21 19" fill="none")
+              path(d="M9.54894 0.927049C9.8483 0.0057385 11.1517 0.0057404 11.4511 0.927051L13.0819 5.9463C13.2158 6.35833 13.5997 6.63729 14.033 6.63729H19.3105C20.2792 6.63729 20.682 7.8769 19.8983 8.4463L15.6287 11.5484C15.2782 11.803 15.1315 12.2544 15.2654 12.6664L16.8963 17.6857C17.1956 18.607 16.1411 19.3731 15.3574 18.8037L11.0878 15.7016C10.7373 15.447 10.2627 15.447 9.91221 15.7016L5.64258 18.8037C4.85887 19.3731 3.80439 18.607 4.10374 17.6857L5.7346 12.6664C5.86847 12.2544 5.72181 11.803 5.37132 11.5484L1.10169 8.4463C0.317977 7.8769 0.720754 6.63729 1.68948 6.63729H6.96703C7.40026 6.63729 7.78421 6.35833 7.91809 5.9463L9.54894 0.927049Z" fill="#6185EE")
+          div.column-2 {{model.status}}
+          div.column-3 {{model.savedVersion}}
+          div.column-4 {{model.sessionEndTime}}
+          div.column-5 Collaborators
+          div.column-6 {{model.lastModified.date}}
+</template>s
 
-</template>
 <script>
-  import {filePCRead, folderPCDelete, deepCopy, projectPathModel}  from '@/core/helpers.js'
-  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
-
-  import imageClassification    from '@/core/basic-template/image-classification.js'
-  import reinforcementLearning  from '@/core/basic-template/reinforcement-learning.js'
-  import timeseriesRegression   from '@/core/basic-template/timeseries-regression.js'
-  import {isWeb} from "@/core/helpers";
-
+  import projectSidebar from '@/pages/layout/project-sidebar.vue';
+  import SortByButton from '@/pages/projects/components/sort-by-button.vue';
+  
   export default {
-    name: 'PageProjects',
-    created() {
-      if(isWeb()) {
-        this.$store.dispatch('mod_workspace/GET_workspacesFromLocalStorage');
-      }
-    },
-    mounted() {
-      this.checkCloudToken()
-    },
-    data() {
+    name: "pageProjects",
+    components: {projectSidebar, SortByButton},
+    data: function () {
       return {
-        source: 'computer',
-        service: '',
-        search: '',
-        projects: [],
-        basicTemplates: [
-          {
-            title: 'Image Classification',
-            imgPath: './static/img/project-page/image-classification.svg',
-            template: imageClassification
-          },
-          {
-            title: 'Timeseries Regression',
-            imgPath: './static/img/project-page/time-series-regression.svg',
-            template: timeseriesRegression
-          },
-          {
-            title: 'Reinforcement Learning',
-            imgPath: './static/img/project-page/reinforcement-learning.svg',
-            template: reinforcementLearning
-          },
+        isSelectedSortType: 0,
+        searchValue: '',
+        sortOptions: [
+          {name: 'Name', value: 1},
+          {name: 'Date Last Opened', value: 2},
+          {name: 'Date Last Modified', value: 3},
+          {name: 'Date Created', value: 4},
+          {name: 'Size', value: 5},
         ],
-        isWeb: isWeb()
-      }
-    },
-    computed: {
-      ...mapGetters({
-        networkIsNotEmpty:  'mod_workspace/GET_networkIsNotEmpty',
-        localUserInfo:      'mod_user/GET_LOCAL_userInfo'
-      }),
-      ...mapState({
-        appVersion:         state => state.globalView.appVersion,
-        hotKeyPressDelete:  state => state.mod_events.globalPressKey.del,
-      }),
-      filteredProjects() {
-        this.selectedProject = null;
-        return this.projects.filter((project)=> project.name.match(this.search))
-      }
-    },
-    watch: {
-      'localUserInfo.projectsList.length': {
-        handler() {
-          if(!this.localUserInfo) return;
-
-          let localProjectsList = deepCopy(this.localUserInfo.projectsList);
-          if (Array.isArray(localProjectsList)) {
-            localProjectsList.forEach((el) => {
-              el.notExist = false;
-              el.isChecked = false;
-              filePCRead(projectPathModel(el.pathProject))
-                .then(() => { })
-                .catch((err) => {
-                  el.notExist = true
-                })
-            });
-            this.projects = localProjectsList;
-          }
-        },
-        immediate: true
-      },
-      hotKeyPressDelete() {
-        const indexCheckedProj = this.projects.findIndex((el)=> el.isChecked === true);
-        if(indexCheckedProj < 0) return;
-
-        const selectedProject = this.projects[indexCheckedProj];
-        //const isProjectNotExist = selectedProject.notExist;
-        const pathDelete = selectedProject.pathProject;
-        folderPCDelete(pathDelete)
-          .then(()=> {
-            const newProjectsList = deepCopy(this.localUserInfo.projectsList);
-            newProjectsList.splice(indexCheckedProj, 1);
-            this.saveLocalUserInfo({key: 'projectsList', data: newProjectsList });
-            this.$nextTick(()=> this.showInfoPopup("The project has been successfully deleted"))
-          })
-          .catch ((err)=> {console.error(err)})
+        modelList: [
+          {id: 1, name:'Placeholder', status: '75%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: true},
+          {id: 2, name:'Placeholder', status: '50%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 3, name:'Placeholder', status: '45%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 4, name:'Placeholder', status: '25%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 5, name:'Placeholder', status: '65%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 6, name:'Placeholder', status: '55%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 7, name:'Placeholder', status: '95%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+          {id: 8, name:'Placeholder', status: '75%', savedVersion: '-', sessionEndTime: 'Placeholder', collaborators: [], lastModified: { collaboratorId: 1, date: '24/02/20 13:00:00'}, isFavorite: false},
+        ],
+        selectedListIds: [1],
       }
     },
     methods: {
-      ...mapMutations({
-        setTutorialMode:      'mod_tutorials/SET_isTutorialMode',
-        setTutorialStoryBoard:'mod_tutorials/SET_showTutorialStoryBoard',
-        restore_network:      'mod_workspace/RESTORE_network',
-      }),
-      ...mapActions({
-        openNetwork:      'mod_events/EVENT_openNetwork',
-        loadNetwork:      'mod_events/EVENT_loadNetwork',
-        beginTutorial:    'mod_tutorials/START_storyboard',
-        addNetwork:       'mod_workspace/ADD_network',
-        saveLocalUserInfo:'mod_user/UPDATE_LOCAL_userInfo',
-        showInfoPopup:    'globalView/GP_infoPopup',
-        checkCloudToken:  'mod_apiCloud/CloudAPI_checkStatus',
-      }),
-      openTemplate(path) {
-        this.loadNetwork(path)
+      onSortByChanged(valueSelected) {
+        // logic for sorting
+        this.isSelectedSortType = valueSelected;
       },
-      selectTemplate(selectEl) {
-        this.projects.forEach((el)=> el.isChecked = false);
-        selectEl.isChecked = true
+      isItemSelected(itemId) {
+        return this.selectedListIds.indexOf(itemId) !== -1;
       },
-      addNewProject() {
-        this.addNetwork()
+      isDisabledCompareBtn() {
+        return this.selectedListIds.length < 2;
       },
-      openBasicTemplate(net) {
-        this.addNetwork(net.network)
+      toggleItemSelection(modelId) {
+        let itmPosition = this.selectedListIds.indexOf(modelId);
+        if(itmPosition === -1) {
+          this.selectedListIds = [...this.selectedListIds, modelId];
+        } else {
+          this.selectedListIds = [...this.selectedListIds.slice(0, itmPosition), ...this.selectedListIds.slice(itmPosition+1)]
+        }
       },
-      openLastWS() {
-        this.restore_network(this.localUserInfo.workspace);
-        this.goNextPage()
+      isAtLeastOneItemSelected() {
+        return this.selectedListIds.length >= 1;
       },
-      goNextPage() {
-        this.$router.push({name: 'app'});
+      removeItems() {
+        let newModelList = [...this.modelList];
+        newModelList = newModelList.filter(item => this.selectedListIds.indexOf(item.id) === -1);
+        this.modelList = newModelList;
+        this.selectedListIds = [];
       },
+      toggleFavoriteItems() {
+        
+      },
+      
     }
   }
 </script>
-<style lang="scss" scoped>
-  @import '../../scss/base';
 
-  $section-indent: 6rem;
-
-  @import './sidebar/sidebar';
-  @import './basic-templates/basic-templates';
-  @import './recent-files/recent-files';
-
-
-  .page-projects {
-    display: grid;
-    background: $bg-workspace;
-    grid-template-columns: $w-sidebar 1fr;
-    grid-template-rows: auto 1fr;
-    grid-template-areas: "sidebar basic-templates"
-                        "sidebar recent-files";
+<style lang="scss">
+  .project-wrapper {
+    margin-left: 40px;
+    background-color: #23252A;
+    height: 100vh;
   }
-
-  .page-projects_sidebar {
-    grid-area: sidebar;
-    padding: 2rem 1rem 1rem 1rem;
-    background: $col-txt2;
-    h3 {
-      margin: 2rem 0;
-    }
+  .title {
+    padding-top: 14px;
+    padding-bottom: 18px;
+    margin-bottom: 0;
+    font-size: 24px;
+    border-bottom: 1px solid #363E51;
   }
-  .projects-sidebar_link {
-    font-size: 1.4rem;
-    width: 15rem;
-    margin-bottom: 2rem;
-    text-align: center;
-    font-weight: normal;
-    > * {
-      vertical-align: middle;
-    }
-  }
-  .page-projects_basic-templates {
-    grid-area: basic-templates;
+  .header-controls {
+    padding: 7px 40px;
+    border-bottom: 1px solid #363E51;;
     display: flex;
-    border-bottom: 1px solid $colorGrey;
-    margin: 0 $section-indent;
-    padding: $section-indent 0;
-    position: relative;
-  }
-  .projects-templates_link {
-    position: absolute;
-    right: 0;
-    top: 1rem;
-  }
-  .page-projects_recent-files {
-    grid-area: recent-files;
-    padding: $section-indent $section-indent $section-indent/2 $section-indent;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-  }
-
-  .page-projects_title {
-    margin-bottom: $section-indent;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    &.get-started-title-margin {
-     margin-bottom: 7rem;
-    }
-    h2 {
+    .left-side {
       display: flex;
-      align-items: center;
-      text-transform: uppercase;
-      font-weight: 500;
-      font-size: 1.8rem;
-      margin-bottom: 0;
     }
-
-    .title-box_projectscount {
-      font-size: 1.2rem;
-      margin-left: 1rem;
+    .right-side {
+      margin-left: auto;
+      display: flex;
     }
   }
-  .page-projects_project-list {
-    flex: 1 1 100%;
-    overflow: auto;
-    margin-right: -1.4rem;
+  .search-input {
+    position: relative;
+    width: 333px;
+    img {
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 10px;
+    }
+    input {
+      padding-left: 44px;
+      background-color: transparent;
+      border: 1px solid #363E51;
+      border-radius: 5px;
+      height: 29px;
+    }
+  }
+  .text-button {
+    cursor: pointer;
+    font-weight: bold;
+    background: transparent;
+    border-radius: 5px;
+    padding: 3px 9px;
+    margin: 0 10px;
+    font-size: 16px;
+    color: #E1E1E1;
+    line-height: 23px;
+    &.is-disable {
+      color: #818181;
+    }
+    &:hover {
+      background: #383F50;
+    }
+  }
+  .img-button {
+    cursor: pointer;
+    margin: 0 10px 0 25px;
+  }
+  .btn-round-icon {
+    cursor: pointer;
+    margin-right: 35px;
+    width: 19px;
+    height: 19px;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-self: center;    
+  }
+  .pl-40 {
+    padding-left: 40px;
+  }
+  
+  .models-list-row {
+    .column-1 {
+      position: relative;
+      margin-right: auto;
+      padding-left: 135px;
+    }
+    .column-2 {
+      min-width: 130px; 
+    }
+    .column-3 {
+      min-width: 200px;
+    }
+    .column-4 {
+      min-width: 220px;
+    }
+    .column-5 {
+      min-width: 180px;
+    }
+    .column-6 {
+      min-width: 180px;
+    }
+  }
+  .model-list-header {
+    display: flex;
+    height: 43px;
+    font-size: 16px;
+    font-weight: bold;
+    border-bottom: 1px solid #363E51;
+    align-items: center;
+  }
+  .model-list-item {
+    display: flex;
+    height: 43px;
+    font-size: 16px;
+    font-weight: 400;
+    border-bottom: 1px solid #363E51;
+    align-items: center;
+    &.is-selected {
+      background: rgba(97, 133, 238, 0.4)
+    }
+    &:hover {
+      background: rgba(97, 133, 238, 0.75);
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      .is-favorite{
+        path {
+          fill: #E1E1E1;
+        }
+      }
+    }
+    
+    .column-1 {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      .btn-round-icon {
+        position: absolute;
+        left: 41px;
+        top: 50%;
+        transform: translateY(-50%)
+      }
+      .is-favorite {
+        position: absolute;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
   }
 </style>
