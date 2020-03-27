e656a21ac8fa3be85ddeab416c85fec38f480587
David Lai <david.l@perceptilabs.com>
2020-03-26 18:53:07 -0700

    Created set of Object Detection Layers

---
+++ frontend/src/renderer/components/network-elements/elements/train-detector/view-train-detector.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .btn.btn--layersbar.net-element-train(:draggable="draggable"
+    data-component="TrainDetector"
+    data-layer="Detector"
+    data-type="Training"
+    v-tooltip:right="'Detector'"
+    v-tooltip-interactive:right="interactiveInfo"
+    id="tutorial_training-detector"
+    :style="layerStyles"
+  )
+    i.icon.icon-training
+
+</template>
+
+<script>
+  import mixinSet from '@/core/mixins/net-element-view-layer.js';
+export default {
+  name: 'ViewTrainDetector',
+  mixins: [mixinSet],
+  data() {
+    return {
+      interactiveInfo: {
+        title: 'Train Detector',
+        text: 'Detector supervised learning layer'
+      }
+    }
+  }
+}
+</script>
