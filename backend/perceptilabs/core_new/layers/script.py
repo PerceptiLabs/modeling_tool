332d9aa8346db7dd696f944654145f810e324763
Anton Kollmats <antonkollmats2@gmail.com>
2020-04-02 00:41:41 +0200

    update script factory output and some refactoring

--- backend/perceptilabs/core_new/layers/script.py
+++ backend/perceptilabs/core_new/layers/script.py
@@ -22,6 +22,13 @@ log = logging.getLogger(__name__)
 class ScriptBuildError(Exception):
     pass
 
+def is_syntax_ok(code):
+    try:
+        ast.parse(code)
+    except SyntaxError as e:
+        return False
+    else:
+        return True
 
 class ScriptFactory:
     def __init__(self, mode='default'):
@@ -32,7 +39,133 @@ class ScriptFactory:
         self._engine = J2Engine(templates_directory)
         self._definition_table = DEFINITION_TABLE
 
-    def make(self, graph: Graph, session_config: Dict[str, str]):
+    def _create_imports_snippet(self, graph):
+        plabs_imports = set([
+            'from perceptilabs.core_new.graph.builder import GraphBuilder, SnapshotBuilder',
+            'from perceptilabs.core_new.communication import TrainingServer',
+            'from perceptilabs.core_new.layers.replication import BASE_TO_REPLICA_MAP, REPLICATED_PROPERTIES_TABLE'                    
+        ])
+        other_imports = set([
+            'import sys',
+            'import logging'
+        ])
+
+        
+        for node in graph.nodes:
+            layer_def = self._definition_table.get(node.layer_type)
+
+            for stmt in layer_def.import_statements:
+                if not is_syntax_ok(stmt):
+                    raise ScriptBuildError(f"Invalid syntax for import statement '{stmt}' found in layer def for {node.layer_type}")
+                
+                if 'perceptilabs' in stmt:
+                    plabs_imports.add(stmt)
+                else:
+                    other_imports.add(stmt)
+
+        code = ''
+        
+        for stmt in sorted(other_imports, key=len):
+            code += stmt + '\n'
+        if len(code) > 0:
+            code += '\n'
+            
+        for stmt in sorted(plabs_imports, key=len):
+            code += stmt + '\n'            
+        if len(code) > 0:
+            code += '\n'
+
+        return code
+
+    def _create_logging_snippet(self):
+        code  = "logging.basicConfig(\n"
+        code += "    stream=sys.stdout,\n"
+        code += "    format='%(asctime)s - %(levelname)s - %(message)s',\n"
+        code += "    level=logging.INFO\n"
+        code += ")\n"
+        code += "log = logging.getLogger(__name__)\n\n"
+        return code        
+    
+    def _create_layers_snippet(self, graph, offset):
+        code = ''
+        line_to_node_map = {}
+        
+        for node in graph.nodes:
+            layer_code = self.render_layer_code(
+                node.layer_id,
+                node.layer_type,
+                node.layer_spec,
+                node.custom_code
+            )
+            code += layer_code + '\n'
+            n_lines = len(layer_code.split('\n'))
+            line_to_node_map.update({offset+line: (node, line) for line in range(n_lines)})
+            offset += n_lines
+
+        if len(code) > 0:
+            code += '\n'
+            
+        return code, line_to_node_map
+
+    def _create_graph_snippet(self, graph):
+        code = "LAYERS = {\n"
+        for node in graph.nodes:
+            layer_name = node.layer_type + node.layer_id
+            code += "    '" + node.layer_id + "': " + layer_name + "(),\n"
+        code += "}\n\n"
+
+        code += "EDGES = {\n"
+        for node in graph.nodes:
+            from_id = node.layer_id
+            for _, to_id in node.layer_spec['forward_connections']:
+                code += "    ('" + from_id + "', '" + sanitize_layer_name(to_id) + "'),\n"
+        code += "}\n\n"
+
+        code += "graph_builder = GraphBuilder()\n"
+        code += "graph = graph_builder.build(LAYERS, EDGES)\n\n"        
+        return code
+
+    def _create_training_server_snippet(self, session_id):
+        code  = "snapshot_builder = SnapshotBuilder(\n"
+        code += "    BASE_TO_REPLICA_MAP, \n"
+        code += "    REPLICATED_PROPERTIES_TABLE\n"
+        code += ")\n"        
+        code += "server = TrainingServer(\n"
+        code += "    6556, 6557,\n" # TODO: use this and sharedmemory for now... https://stackoverflow.com/questions/11341795/zeromq-and-bind-to-random-port-how-to-get-port-chosen
+        code += "    graph,\n"
+        code += "    snapshot_builder=snapshot_builder\n"
+        code += ")\n\n"
+        return code
+
+    def _create_rest_server_snippet(self):
+        return ""
+
+    def _create_main_block(self):
+        code  = "def main():\n"
+        code += "    server.start(block=True)\n"
+        code += "\n"
+        code += "if __name__ == '__main__':\n"
+        #code += "    wait = '--wait' in sys.argv\n"
+        code += "    server.start(block=True)\n"        
+        
+        return code
+
+    def make(self, graph, session_id):
+        code  = self._create_imports_snippet(graph)
+        code += self._create_logging_snippet()
+        
+        # Add the layer classes
+        initial_offset = len(code.split('\n')) - 1
+        layers_code, line_to_node_map = self._create_layers_snippet(graph, initial_offset)
+        code += layers_code
+
+        code += self._create_graph_snippet(graph)
+        code += self._create_training_server_snippet(session_id)
+        code += self._create_rest_server_snippet()
+        code += self._create_main_block()
+        return code, line_to_node_map
+
+    def make_(self, graph: Graph, session_config: Dict[str, str]):
         #imports = {}
         #macro_calls = []
 
