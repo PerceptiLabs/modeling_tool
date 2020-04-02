61d0ea20ebe231323ca71096e8633db816aa2d0b
Robert Lundberg <Lundbergrobert@telia.com>
2020-04-02 11:03:19 +0200

    small fix

--- frontend/src/renderer/components/workspace/workspace-save-net.js
+++ frontend/src/renderer/components/workspace/workspace-save-net.js
@@ -40,7 +40,6 @@ const workspaceSaveNet = {
     ...mapActions({
       infoPopup: 'globalView/GP_infoPopup',
       set_networkRootFolder:'mod_workspace/SET_networkRootFolder',
-      setNetworkName:       'mod_workspace/SET_networkName',
       checkTrainedNetwork:  'mod_api/API_checkTrainedNetwork',
       saveTrainedNetwork:   'mod_api/API_saveTrainedNetwork',
       trackerModelSave:     'mod_tracker/EVENT_modelSave',
@@ -106,8 +105,6 @@ const workspaceSaveNet = {
       const currentNet = this.currentNetwork;
       const newProjectId = netId || generateID();
       const pathSaveProject = netInfo.projectPath;
-
-      this.setNetworkName(netInfo.projectName);
 
       let prepareNet = cloneNet(currentNet, newProjectId, netInfo);
       /*check Is Trained Net + do ScreenShot*/
