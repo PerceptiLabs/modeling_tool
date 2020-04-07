820b19644a043992dbf626441bd1125df12b1482
Anton Kollmats <antonkollmats2@gmail.com>
2020-04-07 21:38:23 +0200

    fix small error

--- backend/conftest.py
+++ backend/conftest.py
@@ -8,7 +8,6 @@ log = logging.getLogger(__name__)
 
 @pytest.fixture(scope='function', autouse=True)
 def print_name_and_memory():
-    resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
     if os.name == 'nt':
@@ -17,7 +16,7 @@ def print_name_and_memory():
         import resource
         rss_max = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024/1024
         log.info('Initializing test: {}. Max RSS: {} [MiB]'.format(test_name, rss_max))
-    
+        
     yield
     log.info('Finalizing test: {}'.format(test_name))    
 
