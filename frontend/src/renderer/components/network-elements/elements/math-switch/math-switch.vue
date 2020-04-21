9b0eddec1ca3fee90dbcae8ab52965fe04836d50
David Lai <david.l@perceptilabs.com>
2020-04-21 10:56:45 -0700

    Created Swtich component basedon Argmax

---
+++ frontend/src/renderer/components/network-elements/elements/math-switch/math-switch.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  base-net-el(
+    :data-el="elementData"
+  )
+    view-el(:current-el="elementData")
+    template(slot="settings")
+      settings-el(:current-el="elementData")
+
+</template>
+
+<script>
+  import BaseNetEl    from '@/components/network-elements/net-base-element/net-base-element.vue';
+  import ViewEl       from './view-math-switch.vue';
+  import SettingsEl   from './set-math-switch';
+
+  export default {
+    name: 'MathSwitch',
+    components: {
+      BaseNetEl,
+      ViewEl,
+      SettingsEl
+    },
+    props: {
+      elementData: Object
+    },
+  }
+</script>
---
+++ frontend/src/renderer/components/network-elements/elements/math-switch/set-math-switch.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  net-base-settings(
+    :current-el="currentEl"
+    @press-apply="saveSettings($event)"
+    @press-confirm="confirmSettings"
+  )
+    template(slot="Settings-content")
+      .settings-layer_section
+        .form_row(v-tooltip-interactive:right="interactiveInfo.dimension")
+          .form_label Dimension:
+          .form_input
+            input(type="text" v-model="settings.Dim")
+    template(slot="Code-content")
+      settings-code(
+        :current-el="currentEl"
+        :el-settings="settings"
+        v-model="coreCode"
+      )
+
+</template>
+
+<script>
+import mixinSet       from '@/core/mixins/net-element-settings.js';
+import TripleInput    from "@/components/base/triple-input";
+
+export default {
+  name: 'SetMathSwitch',
+  mixins: [mixinSet],
+  components: { TripleInput },
+  data() {
+    return {
+      settings: {
+        Dim: -1,
+      },
+      interactiveInfo: {
+        dimension: {
+          title: 'Dimension',
+          text: 'Choose which axis to do the operation on'
+        }
+      }
+    }
+  }
+}
+</script>
