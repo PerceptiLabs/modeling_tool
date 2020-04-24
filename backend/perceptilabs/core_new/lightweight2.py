fix error where lw core printed wrong dims

backend/perceptilabs/core_new/lightweight2.py
@@ -93,9 +93,10 @@ class Tf1xStrategy:
                         break                
 
                 output = outputs.get(layer_id, None)
+                
                 results[layer_id] = LayerInfo(
                     sample=output,
-                    out_shape=np.atleast_2d(output).shape[1:] if output is not None else None,
+                    out_shape=np.atleast_1d(output.squeeze()).shape if output is not None else None,
                     in_shape=None,
                     variables=var_names,
                     default_var=default_var
@@ -125,13 +126,13 @@ class Tf1xStrategy:
             pass
         elif isinstance(layer, DataLayer):
             try:
-                y = tf.constant(layer.sample)            
+                y = tf.constant(layer.sample)
                 output_tensors[layer_id] = y
             except Exception as e:
                 errors[layer_id] = exception_to_error(layer_id, layer_type, e)                    
         elif isinstance(layer, InnerLayer):
             bw_cons = [input_id for input_id, _ in layer_spec['backward_connections']]
-            
+
             args = {}
             for input_id in bw_cons:
                 if input_id in output_tensors:
@@ -216,7 +217,6 @@ class LightweightCore:
                 
         strategy = self._get_subgraph_strategy(subgraph_spec)
         results, strategy_errors = strategy.run(subgraph_spec, ordered_ids, layer_instances, layer_infos)
-
         if self._cache is not None:
             for layer_id, layer_info in results.items():
                 self._cache.put(layer_id, layer_info, code_map, edges_by_id)