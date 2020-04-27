9a6002eab44069017b75baed1dbf6b3fe601ba36
Anton Bourosu <anton@smartdata.solutions>
2020-04-27 19:52:33 +0300

    remove console log

--- frontend/src/renderer/components/network-elements/elements/train-normal/set-train-normal.vue
+++ frontend/src/renderer/components/network-elements/elements/train-normal/set-train-normal.vue
@@ -99,7 +99,6 @@ export default {
   name: 'SetTrainNormal',
   mixins: [ mixinSet ],
   beforeMount() {
-    console.log(this.inputId);
     this.inputId.forEach((id)=> {
       let elList = this.currentNetworkList;
       this.inputLayers.push({
