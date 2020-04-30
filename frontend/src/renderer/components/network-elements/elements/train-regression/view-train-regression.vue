0167a0e5afbacb331a2cd51c23709049253828e4
David Lai <david.l@perceptilabs.com>
2020-04-30 12:51:33 -0700

    created regression component

---
+++ frontend/src/renderer/components/network-elements/elements/train-regression/view-train-regression.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .btn.btn--layersbar.net-element-train(:draggable="draggable"
+    data-component="TrainRegression"
+    data-layer="Regression"
+    data-type="Training"
+    v-tooltip:right="'Regression'"
+    v-tooltip-interactive:right="interactiveInfo"
+    id="tutorial_training-regression"
+    :style="layerStyles"
+  )
+    i.icon.icon-training
+
+</template>
+
+<script>
+  import mixinSet from '@/core/mixins/net-element-view-layer.js';
+export default {
+  name: 'ViewTrainRegression',
+  mixins: [mixinSet],
+  data() {
+    return {
+      interactiveInfo: {
+        title: 'Train Regression',
+        text: 'Regression supervised learning layer'
+      }
+    }
+  }
+}
+</script>
