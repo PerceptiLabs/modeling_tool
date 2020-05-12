257f6e9a09797a997b88ea92b07324d662fc0256
Derek Ly <derek.ly28@gmail.com>
2020-05-12 11:35:37 -0600

    Implemented last_node component

--- backend/perceptilabs/core_new/core2.py
+++ backend/perceptilabs/core_new/core2.py
@@ -316,8 +316,13 @@ class Core:
         self._graphs.append(graph)        
     '''
     def _on_userland_error(self, exception, traceback_frames):
+        import pdb
         message = ''
         collect = False
+        last_node = None
+        last_lineno = None
+
+        #pdb.set_trace()
         for frame in traceback_frames:
             node, true_lineno = self._line_to_node_map.get(frame.lineno, (None, None))
             
@@ -327,21 +332,26 @@ class Core:
                 continue
                 
             if frame.filename == 'training_script.py' and node is not None:
+                last_node = node
+                last_lineno = true_lineno
+
                 message += f'File "{frame.filename}", line {frame.lineno}, in {frame.name}, ' + \
                            f'origin {node.layer_id}:{true_lineno} [{node.layer_type}]\n' +\
                            f'  {frame.line}\n'
             else:
                 message += f'File "{frame.filename}", line {frame.lineno}, in {frame.name}\n' + \
                            f'  {frame.line}\n'
-                    
-        error = UserlandError(node.layer_id, node.layer_type, frame.lineno, message)
+        
+        #pdb.set_trace()
+        error = UserlandError(last_node.layer_id, last_node.layer_type, last_lineno, message)
 
-        if self._use_sentry:
-            with sentry_sdk.push_scope() as scope:
-                scope.set_tag('error-type', 'userland-error')
-                scope.level = 'info'
-                sentry_sdk.capture_message(error.format())
+        #if self._use_sentry:
+        #    with sentry_sdk.push_scope() as scope:
+        #        scope.set_tag('error-type', 'userland-error')
+        #        scope.level = 'info'
+        #        sentry_sdk.capture_message(error.format())
 
+        #pdb.set_trace()
         log.info('Training stopped because of userland error:\n' + error.format())
         if self._issue_handler is not None:
             self._issue_handler.put_error(error.format())
