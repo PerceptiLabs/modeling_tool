d6a057cbe605a7810f654935227712975b6cd130
Jon Phillips <joncp@pm.me>
2020-05-19 15:34:06 -0700

    [514] remove vestigial code

--- Procfile
+++
@@ -1,1 +0,0 @@
-api: ./manage.py runserver
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-        name = "rygg",
-        version="0.0.1",
-        packages=["rygg", "rygg.api"],
-        )
