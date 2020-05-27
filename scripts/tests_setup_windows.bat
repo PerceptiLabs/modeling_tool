ea9d6c189071f56ba9974f0eeae67ee639ddf26e
Robert Lundberg <Lundbergrobert@telia.com>
2020-05-27 11:26:53 +0000

    Merged PR 362: remove upgrade of dask
    remove upgrade of dask

--- scripts/tests_setup_windows.bat
+++ scripts/tests_setup_windows.bat
@@ -2,6 +2,6 @@ echo "Installing dependencies"
 call python -m pip install --upgrade pip setuptools
 call pip install "gym[atari]" 
 call pip install -r ../backend/requirements.txt
-call pip install dask[array] --upgrade
+REM call pip install dask[array] --upgrade
 call pip install pylint==2.4.3
 call pip install pytest==5.3.1
