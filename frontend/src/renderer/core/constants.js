2940d53a3d9de86a389d72adec35da5555f0073d
David Chan <everdev0923@gmail.com>
2020-06-03 11:31:06 +0000

    Merged PR 377: Change base url site
    Related work items: #199, #286, #294, #298, #375, #407, #408, #478, #485, #497, #498, #504, #545, #557

--- frontend/src/renderer/core/constants.js
+++ frontend/src/renderer/core/constants.js
@@ -11,7 +11,8 @@ const pathWebWorkers = './static/webworkers';
 // const baseUrlCloud  = 'http://perceptilabs.trafficmanager.net/api/';
 const baseUrlCloud  = 'https://perceptilabsamerica.azurewebsites.net/api/';
 // const baseUrlCloud  = 'http://localhost:8000/';
-const baseUrlSite   = 'https://perceptilabs-website-dev.azurewebsites.net';
+// const baseUrlSite   = 'https://perceptilabs-website-dev.azurewebsites.net';
+const baseUrlSite   = 'https://perceptilabs.com';
 
 const widthElement = 60;
 const workspaceGrid = 10;
