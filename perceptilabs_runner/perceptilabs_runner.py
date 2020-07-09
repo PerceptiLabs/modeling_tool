87cbc7602443ab59668bd2f6467197671f058f1e
Jon Phillips <joncp@pm.me>
2020-07-09 15:35:09 -0700

    Add missing ERROR flag in perceptilabs_runner

--- perceptilabs_runner/perceptilabs_runner.py
+++ perceptilabs_runner/perceptilabs_runner.py
@@ -12,6 +12,7 @@ class bcolors:
     KERNEL = '\033[95m'
     PERCEPTILABS = '\033[94m'
     RYGG = '\033[92m'
+    ERROR = '\033[31m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
