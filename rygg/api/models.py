e0a69ca5f61cf9a1088d578ddc05ee661e10dd48
David Han <david_han@hotmail.com>
2020-07-20 11:39:34 -0700

    Adds models

--- rygg/api/models.py
+++ rygg/api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class FileLink(models.Model):
+    filelink_id = models.AutoField(primary_key=True)
+    resource_locator = models.CharField(max_length=1000, blank=False)
+
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=1000, blank=False)
@@ -8,6 +12,8 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.name} ({self.project_id})"
 
 class Model(models.Model):
     project = models.ForeignKey(
@@ -20,3 +26,18 @@ class Model(models.Model):
     updated = models.DateTimeField(auto_now=True)
     saved_by = models.CharField(max_length=100, blank=True)
     saved_version_location = models.CharField(max_length=100, blank=True)
+
+class Notebook(models.Model):
+    project = models.ForeignKey(
+        Project, related_name="notebooks", on_delete=models.PROTECT
+    )
+    filelink = models.ForeignKey(
+        FileLink, related_name="notebook", on_delete=models.SET_NULL, null=True
+    )
+    notebook_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1000, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.notebook_id})"
