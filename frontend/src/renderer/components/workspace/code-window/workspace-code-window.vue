c05f80b571d954f74187b305df29ffa43243508c
Anton Bourosu <anton@smartdata.solutions>
2021-03-09 13:23:55 +0000

    Merged PR 1106: Minimize and maximize code window
    This pr make posible to maximize and minimize back the code editor.
    
    Related work items: #893

--- frontend/src/renderer/components/workspace/code-window/workspace-code-window.vue
+++ frontend/src/renderer/components/workspace/code-window/workspace-code-window.vue
@@ -14,6 +14,12 @@
           span {{ this.currentEl.layerName }}
           span(v-if="hasUnsavedCodeChanges") *
         div
+          svg.minimize-maximize(
+            @click="toggleFullView"
+            xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewbox='0 0 12 12' fill='none')
+            path(d='M1 8H3.75C3.88807 8 4 8.11193 4 8.25V11' stroke='#E1E1E1' stroke-width='0.75')
+            path(d='M4.33333 1H1.25C1.11193 1 1 1.11193 1 1.25V10.75C1 10.8881 1.11193 11 1.25 11H10.75C10.8881 11 11 10.8881 11 10.75V7.66667' stroke='#E1E1E1' stroke-width='0.75' stroke-linecap='round')
+            path(d='M6.41161 5.21729C6.41161 5.4244 6.57951 5.59229 6.78661 5.59229L10.1616 5.59229C10.3687 5.59229 10.5366 5.4244 10.5366 5.21729C10.5366 5.01019 10.3687 4.84229 10.1616 4.84229H7.16161V1.84229C7.16161 1.63519 6.99372 1.46729 6.78661 1.46729C6.57951 1.46729 6.41161 1.63519 6.41161 1.84229L6.41161 5.21729ZM10.7348 0.738741L6.52145 4.95213L7.05178 5.48246L11.2652 1.26907L10.7348 0.738741Z' fill='#E1E1E1')
           svg.close-window(
             @click="beforeCloseWindowCheck"
             viewBox="0 0 7 8"
@@ -31,7 +37,8 @@
               fill="white")
       spinner(v-if="isLoading")
 
-      codeEditor.code-window-content(
+      code-editor.code-window-content(
+        ref="codeEditor"
         v-else
         :code="layerCode"
         :error-row="errorRow"
@@ -152,14 +159,18 @@ export default {
       this.layerCode = objCode.Output;
     },
     toggleFullView() {
-      this.fullView = !this.fullView;
-      document.querySelector('.popup_body').classList.toggle("popup_body--show-code");
-      document.querySelector('.network').classList.toggle("network--show-code");
-      this.$refs.codeEditor.refresh();
+      if(this.fullView) {
+        this.closeFullView();
+      } else {
+        this.fullView = true;
+        document.querySelector('.network').classList.toggle("network--show-code");
+        this.$nextTick(() => this.$refs.codeEditor.handleResize())
+      }  
     },
     closeFullView() {
       this.fullView = false;
       document.querySelector('.network').classList.remove("network--show-code");
+      this.$nextTick(() => this.$refs.codeEditor.handleResize())
     },
     beforeCloseWindowCheck() {
       if (this.hasUnsavedChanges) {
@@ -255,11 +266,17 @@ export default {
 
   $code-window-font-size: 1.2rem;
 
-  svg {
-    height: 0.8rem;
-    width: 0.8rem;
+  svg.close-window {
+    margin-top: 1px;
+    height: 1rem;
+    width: 1rem;
   }
-
+  svg.minimize-maximize {
+    cursor: pointer;
+    margin-top: 1px;
+    height: 1.1rem;
+    width: 1rem;
+  }
   .code-window-container {
     position: absolute;
     width: $code-window-width;
@@ -378,6 +395,7 @@ export default {
     flex: 1;
     flex-direction: column;
     width: 100%;
+    height: 100%;
     overflow: hidden;
     .code-wrap,
     .bookmark_content {
