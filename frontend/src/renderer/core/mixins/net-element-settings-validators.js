1bcc55767a93438477dcb57c761c149704de80ef
Anton Bourosu <anton@smartdata.solutions>
2021-04-12 08:16:14 +0000

    Merged PR 1167: Fix settings saving
    # Implementation Details
    -------------------------------
    ## What was changed?
    It was fixed deep learning components settings.
    
    Fix settings saving
    
    Related work items: #1739

--- frontend/src/renderer/core/mixins/net-element-settings-validators.js
+++ frontend/src/renderer/core/mixins/net-element-settings-validators.js
@@ -1,8 +1,13 @@
 const netElementSettingsValidators = {
   computed: {
     isValidKeepProbability() {
+      const dropout =  this.settings.Dropout;
       const value = this.settings.Keep_prob;
-
+      
+      if(!dropout) { 
+        return true;
+      }
+      
       if (!value || value <= 0 || value > 1) {
         return false;
       } else {
