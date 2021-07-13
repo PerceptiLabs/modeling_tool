1a22fc60daf21d973c0809246e5f00762f616557
Robert Lundberg <Lundbergrobert@telia.com>
2021-07-13 14:48:51 +0000

    Merged PR 1395: Revert 'a image encoder recommendation ith 3 convolution layers'
    # Implementation Summary
    -------------------------------
    *Here you can write about how the change affect the users and/or the developers.*
    *You can also add details of **what** was changed, **why** it was changed and **how** it was changed.*
    
    # [Optional] How to review
    -------------------------------
    N/A
    
    # [Optional] Please review the following files
    -------------------------------
    N/A
    
    # What did you do to make the codebase a better place? How did you make the improvements durable
    -------------------------------
    *Here you should describe what improvements you made to codebase while working on the ticket.*
    *Note that the improvements should be proportional in size to the main objective of the ticket*
    
    # Please also:
    - [ ] Include any new files in included_files.txt
    - [ ] Update the story on the board
    - [ ] Announce changes that will affect everyone in #dev in slack
    
    # Implementation Summary
    -------------------------------
    Earlier we only had a single conv layer in the recommended image encoder and it didn't work well with most datasets. After experimenting with few models and datasets, we're increasing the complexity of the image encoder with 3 conv layers that has shown promising performance on mnist, humanactivity, covid19 xray, blood cells, wildfires datasets.
    
    # [Optional] How to review
    -------------------------------
    Load a dataset with image input and the recommended model should have 3 conv layers and a dense layer in the encoder
    
    # [Optional] Please review the following files
    -------------------------------
    all
    
    # What did you do to make the codebase a better place? How did you make the improvements durable
    -------------------------------
    -
    
    # Please also:
    - [x] Include any new files in included_files.txt
    - [x] Update the story on the board
    - [x] Announce changes that will affect everyone in #dev in slack
    
    Reverts !1386
    
    Related work items: #2220

--- backend/perceptilabs/automation/modelrecommender/encoders/image.py
+++ backend/perceptilabs/automation/modelrecommender/encoders/image.py
@@ -16,51 +16,18 @@ class ImageEncoderBlueprint(EncoderBlueprint):
         """
         id1 = builder.add_layer(
             'IoInput',
-            settings={'feature_name': feature_name, 'file_path': feature_spec.file_path, 'datatype': feature_spec.datatype})                
-        id2=builder.add_layer('DeepLearningConv', settings={'conv_type': 'Conv',
-                                                            'conv_dim': '2D',
-                                                            'patch_size': 3,
-                                                            'feature_maps': 64,
-                                                            'stride': 1,
-                                                            'activation': 'ReLU',
-                                                            'batch_norm': True,
-                                                            'padding': 'VALID',
-                                                            'pool': True,
-                                                            'pooling': 'Max',
-                                                            'pool_padding': 'VALID',
-                                                            'pool_area': 2,
-                                                            'pool_stride': 2})
-        id3=builder.add_layer('DeepLearningConv', settings={'conv_type': 'Conv',
-                                                            'conv_dim': '2D',
-                                                            'patch_size': 3,
-                                                            'feature_maps': 64,
-                                                            'stride': 1,
-                                                            'activation': 'ReLU',
-                                                            'padding': 'VALID',
-                                                            'pool': True,
-                                                            'pooling': 'Max',
-                                                            'pool_padding': 'VALID',
-                                                            'pool_area': 2,
-                                                            'pool_stride': 2})
-        id4=builder.add_layer('DeepLearningConv', settings={'conv_type': 'Conv',
-                                                            'conv_dim': '2D',
-                                                            'patch_size': 3,
-                                                            'feature_maps': 64,
-                                                            'stride': 1,
-                                                            'activation': 'ReLU',
-                                                            'padding': 'VALID',
-                                                            'pool': True,
-                                                            'pooling': 'Max',
-                                                            'pool_padding': 'VALID',
-                                                            'pool_area': 2,
-                                                            'pool_stride': 2})
-        id5=builder.add_layer('DeepLearningFC',
-                                settings={'n_neurons': 128, 'activation': 'ReLU', 'batch_norm': True})
+            settings={'feature_name': feature_name, 'file_path': feature_spec.file_path, 'datatype': feature_spec.datatype}                
+        )
+        id2 = builder.add_layer(
+            'DeepLearningConv'
+        )
+        id3 = builder.add_layer(
+            'DeepLearningFC',
+            settings={'n_neurons': 128, 'activation': 'ReLU'}
+        )
         builder.add_connection(id1, 'output', id2, 'input')
         builder.add_connection(id2, 'output', id3, 'input')
-        builder.add_connection(id3, 'output', id4, 'input')
-        builder.add_connection(id4, 'output', id5, 'input')
-        return id5
+        return id3
         
     
     
