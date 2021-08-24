002b79614511592a83da3ee3577f03822d0594c8
David Chan <everdev0923@gmail.com>
2021-08-24 14:29:41 +0000

    Merged PR 1453: Fix questionnaire not close for demos
    # Implementation Summary
    -------------------------------
    *Here you can write about how the change affect the users and/or the developers.*
    *You can also add details of **what** was changed, **why** it was changed and **how** it was changed.*
    
    # [Optional] How to review
    -------------------------------
    N/A
    
    # [Optional] Please review the following files
    -------------------------------
    N/A
    
    # What did you do to make the codebase a better place? How did you make the improvements durable
    -------------------------------
    *Here you should describe what improvements you made to codebase while working on the ticket.*
    *Note that the improvements should be proportional in size to the main objective of the ticket*
    
    # Please also:
    - [ ] Include any new files in included_files.txt
    - [ ] Update the story on the board
    - [ ] Announce changes that will affect everyone in #dev in slack
    
    Catch error when KC is not available
    
    Related work items: #2316

--- frontend/src/renderer/core/apiKeyCloak.js
+++ frontend/src/renderer/core/apiKeyCloak.js
@@ -1,29 +1,31 @@
-import axios            from 'axios';
-import store            from '@/store';
+import axios from 'axios';
+import store from '@/store';
 
-import { KEYCLOAK_URL_CONFIG_PATH, KEYCLOAK_REALM_PATH }   from "@/core/constants";
+import { KEYCLOAK_URL_CONFIG_PATH, KEYCLOAK_REALM_PATH } from "@/core/constants";
 import { whenUrlIsResolved } from '@/core/urlResolver';
 
 const whenKeyCloakReady = () =>
-  whenUrlIsResolved(KEYCLOAK_URL_CONFIG_PATH, process.env.KEYCLOAK_BASE_URL)
-    .then(url => {
-      let ret = axios.create();
-      ret.defaults.baseURL = url
-      ret.defaults.headers.common["Content-Type"] = `application/json`;
-      ret.defaults.headers.common['Authorization'] = `Bearer ${store.getters['mod_user/GET_userToken']}`;
-      ret.defaults.params = {}
-      return ret
-    });
+  process.env.NO_KC === 'true' ? Promise.reject() :
+    whenUrlIsResolved(KEYCLOAK_URL_CONFIG_PATH, process.env.KEYCLOAK_BASE_URL)
+      .then(url => {
+        let ret = axios.create();
+        ret.defaults.baseURL = url
+        ret.defaults.headers.common["Content-Type"] = `application/json`;
+        ret.defaults.headers.common['Authorization'] = `Bearer ${store.getters['mod_user/GET_userToken']}`;
+        ret.defaults.params = {}
+        return ret
+      });
 
 export const keyCloak = {
-  url(){
+  url() {
     return whenUrlIsResolved(KEYCLOAK_URL_CONFIG_PATH, process.env.KEYCLOAK_BASE_URL)
   },
   updateUserProfileAttributes({ payload }) {
     return whenKeyCloakReady()
       .then(requestor => {
         return requestor.post(`${KEYCLOAK_REALM_PATH}/account/`, payload)
-      });
+      })
+      .catch(console.log);
   },
   isReachable() {
     return whenKeyCloakReady()
