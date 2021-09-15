931be674555791db074eb9d405a68a7103540749
Anton Bourosu <anton@smartdata.solutions>
2021-09-15 03:02:15 +0000

    Merged PR 1478: Make more obivios intercom button, cleaning up component.
    # Implementation Summary
    -------------------------------
    *Here you can write about how the change affect the users and/or the developers.*
    *You can also add details of **what** was changed, **why** it was changed and **how** it was changed.*
    
    # [Optional] How to review
    -------------------------------
    N/A
    
    # [Optional] Please review the following files
    -------------------------------
    N/A
    
    # What did you do to make the codebase a better place? How did you make the improvements durable
    -------------------------------
    Had cleaned up unnecessary things.
    
    # Please also:
    - [ ] Include any new files in included_files.txt
    - [ ] Update the story on the board
    - [ ] Announce changes that will affect everyone in #dev in slack
    
    Make more obivios intercom button, cleaning up component.
    
    Related work items: #2202

---
+++ frontend/src/renderer/components/app-header/app-header-link.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .help-button-panel-content-item(@click="$emit('handleClick')") 
+    svg(width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg")
+      path(d="M11.3995 1.0009C11.3995 0.724753 11.1756 0.500896 10.8995 0.500896L6.39949 0.500896C6.12335 0.500896 5.89949 0.724754 5.89949 1.0009C5.8995 1.27704 6.12335 1.5009 6.39949 1.5009L10.3995 1.5009L10.3995 5.5009C10.3995 5.77704 10.6234 6.0009 10.8995 6.0009C11.1756 6.0009 11.3995 5.77704 11.3995 5.5009L11.3995 1.0009ZM1.35355 11.2539L11.253 1.35445L10.5459 0.647342L0.646447 10.5468L1.35355 11.2539Z" fill="white")
+    span.link-text {{name}}
+</template>
+<script>
+export default {
+  name: 'AppHeaderLink',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.help-button-panel-content-item {
+  margin: 0;
+  font-family: Nunito Sans;
+  font-size: 14px;
+  line-height: 19px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+}
+
+.help-button-panel-content-item + .help-button-panel-content-item {
+  margin-top: 1rem;
+}
+.link-text {
+  margin-left: 2rem;
+}
+</style>
--- frontend/src/renderer/components/app-header/app-header.vue
+++ frontend/src/renderer/components/app-header/app-header.vue
@@ -6,30 +6,16 @@
       the-menu
     
     .app-header-section.app-header-mid
-      h4(v-if="projectName").page-title <span class="page-route-title">{{routeHeaderAlias}} </span>
-      //- h4(v-if="projectName").page-title {{projectName}} / <span class="page-route-title">{{routeHeaderAlias}} </span>
-
+      h4(v-if="projectName").page-title
+        span.page-route-title {{routeHeaderAlias}} 
+    
     .app-header-section.app-header-right
-
       .help-button(
-        @click="toggleCommunity(!showCommunityPanel)"
-        @blur="toggleCommunity(false)"
+        @click="onToggleIntercom"
         tabindex="0"
         )        
-          
         svg(width="22" height="22" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg")
           path(fill-rule="evenodd" clip-rule="evenodd" d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM4.43934 5.43934C4.15804 5.72064 4 6.10218 4 6.5V15.0948C4.00002 15.169 4.02204 15.2415 4.0633 15.3032C4.10455 15.3648 4.16318 15.4129 4.23175 15.4412C4.30032 15.4696 4.37577 15.4769 4.44853 15.4624C4.52129 15.4478 4.5881 15.412 4.6405 15.3595L6.78025 14.2198C6.92087 14.0791 7.1116 14 7.3105 14H14.5C14.8978 14 15.2794 13.842 15.5607 13.5607C15.842 13.2794 16 12.8978 16 12.5V6.5C16 6.10218 15.842 5.72064 15.5607 5.43934C15.2794 5.15804 14.8978 5 14.5 5H5.5C5.10218 5 4.72064 5.15804 4.43934 5.43934ZM6.35984 7.35984C6.43016 7.28951 6.52554 7.25 6.625 7.25H13.375C13.4745 7.25 13.5698 7.28951 13.6402 7.35984C13.7105 7.43016 13.75 7.52554 13.75 7.625C13.75 7.72446 13.7105 7.81984 13.6402 7.89017C13.5698 7.96049 13.4745 8 13.375 8H6.625C6.52554 8 6.43016 7.96049 6.35984 7.89017C6.28951 7.81984 6.25 7.72446 6.25 7.625C6.25 7.52554 6.28951 7.43016 6.35984 7.35984ZM6.35984 9.23483C6.43016 9.16451 6.52554 9.125 6.625 9.125H13.375C13.4745 9.125 13.5698 9.16451 13.6402 9.23483C13.7105 9.30516 13.75 9.40054 13.75 9.5C13.75 9.59946 13.7105 9.69484 13.6402 9.76517C13.5698 9.83549 13.4745 9.875 13.375 9.875H6.625C6.52554 9.875 6.43016 9.83549 6.35984 9.76517C6.28951 9.69484 6.25 9.59946 6.25 9.5C6.25 9.40054 6.28951 9.30516 6.35984 9.23483ZM6.35984 11.1098C6.43016 11.0395 6.52554 11 6.625 11H10.375C10.4745 11 10.5698 11.0395 10.6402 11.1098C10.7105 11.1802 10.75 11.2755 10.75 11.375C10.75 11.4745 10.7105 11.5698 10.6402 11.6402C10.5698 11.7105 10.4745 11.75 10.375 11.75H6.625C6.52554 11.75 6.43016 11.7105 6.35984 11.6402C6.28951 11.5698 6.25 11.4745 6.25 11.375C6.25 11.2755 6.28951 11.1802 6.35984 11.1098Z" fill="#B6C7FB")
-
-        .help-button-panel(v-if="showCommunityPanel")
-          .help-button-panel-content
-            .help-button-panel-content-item(@click="onOpenYouTube") 
-              span YouTube
-            .help-button-panel-content-item(@click="onOpenSlack") 
-              span Slack
-            .help-button-panel-content-item(@click="onOpenForum") 
-              span Forum
-            .help-button-panel-content-item(@click="onOpenBlogs") 
-              span Blogs
 
       .help-button(
           @click="goToReport"
@@ -43,65 +29,73 @@
         @click="toggleHelp(!showHelpPanel)"
         @blur="toggleHelp(false)"
         tabindex="0"
-        )
-        svg(
-          :data-tutorial-target="'tutorial-model-hub-question-mark'" width="22" height="22" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"
-        )
+      )
+        svg(data-tutorial-target="tutorial-model-hub-question-mark" width="22" height="22" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg")
           path(fill-rule="evenodd" clip-rule="evenodd" d="M20 10C20 12.6522 18.9464 15.1957 17.0711 17.0711C15.1957 18.9464 12.6522 20 10 20C7.34784 20 4.8043 18.9464 2.92893 17.0711C1.05357 15.1957 0 12.6522 0 10C0 7.34784 1.05357 4.8043 2.92893 2.92893C4.8043 1.05357 7.34784 0 10 0C12.6522 0 15.1957 1.05357 17.0711 2.92893C18.9464 4.8043 20 7.34784 20 10ZM6.87 7.54125C6.82971 7.5418 6.78973 7.53412 6.7525 7.51868C6.71528 7.50324 6.6816 7.48038 6.65352 7.45148C6.62543 7.42257 6.60354 7.38825 6.58918 7.3506C6.57482 7.31295 6.5683 7.27276 6.57 7.2325C6.6875 5.11375 8.42125 4.375 10.0063 4.375C11.7513 4.375 13.3463 5.2875 13.3463 7.175C13.3463 8.525 12.5525 9.1675 11.7912 9.74625C10.87 10.445 10.5288 10.7063 10.5288 11.6038V11.735C10.5288 11.8179 10.4958 11.8974 10.4372 11.956C10.3786 12.0146 10.2991 12.0475 10.2163 12.0475H9.20375C9.12173 12.0475 9.04299 12.0153 8.98453 11.9577C8.92606 11.9002 8.89256 11.822 8.89125 11.74L8.88625 11.4688C8.83875 10.31 9.505 9.59625 10.3463 8.985C11.0838 8.43 11.5525 8.065 11.5525 7.27125C11.5525 6.24 10.7675 5.81125 9.91 5.81125C8.90625 5.81125 8.34375 6.40875 8.2325 7.22875C8.21 7.4 8.0725 7.54125 7.9 7.54125H6.86875H6.87ZM9.77625 15.595C9.04625 15.595 8.515 15.1025 8.515 14.4362C8.515 13.7463 9.04625 13.2613 9.7775 13.2613C10.5387 13.2613 11.0625 13.7463 11.0625 14.4362C11.0625 15.1025 10.5375 15.595 9.77625 15.595Z" fill="#B6C7FB")
-
         .help-button-panel(v-if="showHelpPanel")
           .help-button-panel-content
             .help-button-panel-content-item(@click="startUserFlow") Get Started
-            //.help-button-panel-content-item(@click="toggleTutorialTips") {{ hideTipsDisplayText }}
-            //.help-button-panel-content-item(@click="onActivateChecklist") Get started checklist
-            //- .help-button-panel-content-item(@click="goToWhatsNew") What's new
+            app-header-link(
+              name="Tutorials"
+              @handleClick="openVideoTutorials"
+            )
+            app-header-link(
+              name="Documentation"
+              @handleClick="goToDocumentation"
+            )
             hr.help-button-panel-content-item
-            .help-button-panel-content-item(@click="openVideoTutorials") 
-              svg(width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg")
-                path(d="M11.3995 1.0009C11.3995 0.724753 11.1756 0.500896 10.8995 0.500896L6.39949 0.500896C6.12335 0.500896 5.89949 0.724754 5.89949 1.0009C5.8995 1.27704 6.12335 1.5009 6.39949 1.5009L10.3995 1.5009L10.3995 5.5009C10.3995 5.77704 10.6234 6.0009 10.8995 6.0009C11.1756 6.0009 11.3995 5.77704 11.3995 5.5009L11.3995 1.0009ZM1.35355 11.2539L11.253 1.35445L10.5459 0.647342L0.646447 10.5468L1.35355 11.2539Z" fill="white")
-              span Tutorials
-            .help-button-panel-content-item(@click="goToDocumentation") 
-              svg(width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg")
-                path(d="M11.3995 1.0009C11.3995 0.724753 11.1756 0.500896 10.8995 0.500896L6.39949 0.500896C6.12335 0.500896 5.89949 0.724754 5.89949 1.0009C5.8995 1.27704 6.12335 1.5009 6.39949 1.5009L10.3995 1.5009L10.3995 5.5009C10.3995 5.77704 10.6234 6.0009 10.8995 6.0009C11.1756 6.0009 11.3995 5.77704 11.3995 5.5009L11.3995 1.0009ZM1.35355 11.2539L11.253 1.35445L10.5459 0.647342L0.646447 10.5468L1.35355 11.2539Z" fill="white")
-              span Documentation
-            .help-button-panel-content-item#intercom-launcher(@click="onToggleIntercom") 
-              svg(width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg")
-                path(d="M1.75 0.875C1.51794 0.875 1.29538 0.967187 1.13128 1.13128C0.967187 1.29538 0.875 1.51794 0.875 1.75V8.75C0.875 8.98207 0.967187 9.20462 1.13128 9.36872C1.29538 9.53281 1.51794 9.625 1.75 9.625H10.1378C10.6018 9.6251 11.0469 9.80954 11.375 10.1378L13.125 11.8878V1.75C13.125 1.51794 13.0328 1.29538 12.8687 1.13128C12.7046 0.967187 12.4821 0.875 12.25 0.875H1.75ZM12.25 0C12.7141 0 13.1592 0.184375 13.4874 0.512563C13.8156 0.840752 14 1.28587 14 1.75V12.9439C14 13.0304 13.9743 13.1151 13.9262 13.187C13.878 13.259 13.8096 13.315 13.7296 13.3481C13.6496 13.3812 13.5616 13.3898 13.4767 13.3728C13.3918 13.3558 13.3139 13.314 13.2528 13.2528L10.7564 10.7564C10.5923 10.5923 10.3698 10.5001 10.1378 10.5H1.75C1.28587 10.5 0.840752 10.3156 0.512563 9.98744C0.184374 9.65925 0 9.21413 0 8.75V1.75C0 1.28587 0.184374 0.840752 0.512563 0.512563C0.840752 0.184375 1.28587 0 1.75 0L12.25 0Z" fill="white")
-                path(d="M4.375 5.25C4.375 5.48206 4.28281 5.70462 4.11872 5.86872C3.95462 6.03281 3.73206 6.125 3.5 6.125C3.26794 6.125 3.04538 6.03281 2.88128 5.86872C2.71719 5.70462 2.625 5.48206 2.625 5.25C2.625 5.01794 2.71719 4.79538 2.88128 4.63128C3.04538 4.46719 3.26794 4.375 3.5 4.375C3.73206 4.375 3.95462 4.46719 4.11872 4.63128C4.28281 4.79538 4.375 5.01794 4.375 5.25ZM7.875 5.25C7.875 5.48206 7.78281 5.70462 7.61872 5.86872C7.45462 6.03281 7.23206 6.125 7 6.125C6.76794 6.125 6.54538 6.03281 6.38128 5.86872C6.21719 5.70462 6.125 5.48206 6.125 5.25C6.125 5.01794 6.21719 4.79538 6.38128 4.63128C6.54538 4.46719 6.76794 4.375 7 4.375C7.23206 4.375 7.45462 4.46719 7.61872 4.63128C7.78281 4.79538 7.875 5.01794 7.875 5.25ZM11.375 5.25C11.375 5.48206 11.2828 5.70462 11.1187 5.86872C10.9546 6.03281 10.7321 6.125 10.5 6.125C10.2679 6.125 10.0454 6.03281 9.88128 5.86872C9.71719 5.70462 9.625 5.48206 9.625 5.25C9.625 5.01794 9.71719 4.79538 9.88128 4.63128C10.0454 4.46719 10.2679 4.375 10.5 4.375C10.7321 4.375 10.9546 4.46719 11.1187 4.63128C11.2828 4.79538 11.375 5.01794 11.375 5.25Z" fill="white")
-              span Chat with us
-
+            .help-button-panel-content-item.cursor-default External resources
+            app-header-link(
+              name="YouTube"
+              @handleClick="onOpenYouTube"
+            )
+            app-header-link(
+              name="Slack"
+              @handleClick="onOpenSlack"
+            )
+            app-header-link(
+              name="Forum"
+              @handleClick="onOpenForum"
+            )
+            app-header-link(
+              name="Blog"
+              @handleClick="onOpenBlogs"
+            )
       header-profile(v-if="showProfile")
 </template>
-
 <script>
-  import TheMenu from '@/components/the-menu.vue'
-  import HeaderProfile from "@/components/app-header/header-profile";
-  import { mapGetters, mapActions } from 'vuex';
-  import { PERCEPTILABS_DOCUMENTATION_URL, PERCEPTILABS_YOUTUBE_VIDEO_TUTORIAL_URL, PERCEPTILABS_YOUTUBE_URL, PERCEPTILABS_SLACK_URL, PERCEPTILABS_FORUM_URL, PERCEPTILABS_BLOGS_URL, USER_FLOW_CONTENT_ID } from "@/core/constants";
-  import userflow from "userflow.js";
+import TheMenu from '@/components/the-menu.vue';
+import HeaderProfile from "@/components/app-header/header-profile.vue";
+import AppHeaderLink from '@/components/app-header/app-header-link.vue';
+import userflow from "userflow.js";
+import { mapGetters, mapActions } from 'vuex';
+import {
+  PERCEPTILABS_DOCUMENTATION_URL,
+  PERCEPTILABS_YOUTUBE_VIDEO_TUTORIAL_URL,
+  PERCEPTILABS_YOUTUBE_URL,
+  PERCEPTILABS_SLACK_URL,
+  PERCEPTILABS_FORUM_URL,
+  PERCEPTILABS_BLOGS_URL,
+  USER_FLOW_CONTENT_ID,
+} from "@/core/constants";
+
 export default {
   name: "HeaderWin",
-  components: {HeaderProfile, TheMenu},
+  components: {
+    HeaderProfile,
+    TheMenu,
+    AppHeaderLink,
+  },
   data: function() {
     return {
       showHelpPanel: false,
-      showCommunityPanel: false,
       showProfile: !process.env.NO_KC,
-      // MODAL_PAGE_WHATS_NEW,
-    }
+    };
   },
   computed: {
     ...mapGetters({
       currentViewType: 'mod_workspace/GET_viewType',
-      currentModel:    'mod_workspace/GET_currentNetwork',
-      showTips:        'mod_tutorials/getShowTutorialTips',
     }),
-    showRestoreIcon() {
-      return this.$store.state.globalView.appIsFullView
-    },
-    pageTitle() {
-      return this.$store.state.globalView.pageTitle;
-    },
     currentProject() {
       return this.$store.state.mod_project.currentProject;
     },
@@ -111,8 +105,8 @@ export default {
     projectName() {
       const { currentProject, projectsList } = this;
       if(!currentProject) return '';
-      let project = projectsList.filter(project => project.project_id === currentProject)[0];
-      return project ? project.name : ''
+      let project = projectsList.filter((project) => project.project_id === currentProject)[0];
+      return project ? project.name : '';
     },
     routeHeaderAlias() {
       let theName = '';
@@ -133,32 +127,13 @@ export default {
       }
       return theName;
     },
-    hideTipsDisplayText() {
-      if (this.showTips) {
-        return 'Disable tips';
-      } else {
-        return 'Enable tips';
-      }
-    },
   },
   methods: {
     ...mapActions({
-      setNextStep:          'mod_tutorials/setNextStep',
-      setCurrentView:       'mod_tutorials/setCurrentView',
-      activateChecklist:    'mod_tutorials/activateChecklist',
-      setShowChecklist:     'mod_tutorials/setShowChecklist',
-      setTips:              'mod_tutorials/setTutorialNotificationsState',
-      trackHelpOption:      'mod_tracker/TRACK_helpOption',
-      setActivePageAction:  'modal_pages/setActivePageAction',
-      openErrorPopup:       'globalView/GP_infoPopup',
-      trackQuestionMark:    'mod_tracker/TRACK_questionMark',
-      trackIntercom:        'mod_tracker/TRACK_toggleIntercom',
+      trackHelpOption: 'mod_tracker/TRACK_helpOption',
+      trackQuestionMark: 'mod_tracker/TRACK_questionMark',
+      trackIntercom: 'mod_tracker/TRACK_toggleIntercom',
     }),
-    toProjectPage() {
-      if(this.$route.name === 'app') {
-        this.$router.push({name: 'projects'})
-      }
-    },
     goToReport() {
       window.open(PERCEPTILABS_FORUM_URL);
     },
@@ -167,22 +142,6 @@ export default {
       if (value) {
         this.trackQuestionMark()
       }
-    },
-    toggleCommunity(value = null) {
-      this.showCommunityPanel = !!value;
-    },
-    toggleTutorialTips() {
-      this.setTips(!this.showTips);
-      this.trackHelpOption('Toggle tutorial tips');
-    },
-    onActivateChecklist() {
-      this.setShowChecklist();
-      this.activateChecklist();
-      this.trackHelpOption('Activate checklist');
-    },
-    goToWhatsNew() {
-      // this.setActivePageAction(MODAL_PAGE_WHATS_NEW);
-      // this.trackHelpOption('Go to Whats New');
     },
     openVideoTutorials() {
       this.trackHelpOption('Go to Video Tutorials');
@@ -215,11 +174,10 @@ export default {
     },
     startUserFlow(){
       userflow.start(USER_FLOW_CONTENT_ID);
-    }
-  }
-}
+    },
+  },
+};
 </script>
-
 <style lang="scss" scoped>
   @import "../../scss/base";
   .app-header {
@@ -234,35 +192,29 @@ export default {
     box-sizing: border-box;
     border-radius: 0px;
   }
-
   .app-header-section {
     display: flex;
     flex: 1 1 100%;
     align-items: center;
-
     &.app-header-right {
       > *:first-child {
         margin-left: auto;
         display: flex;
         align-items: center;
       }
-
       > *:last-child {
         margin-right: 4rem;
       }
-
       * + * {
         margin-left: 2rem;
       }
     }
   }
-
   .d-none {
     display: none;
   }
   .app-header_logo {
     margin: 0 12px 0 5px;
-    // cursor: pointer;
     a {
       display: block;
       -webkit-app-region: no-drag;
@@ -277,15 +229,11 @@ export default {
     font-size: 16px;
     line-height: 22px;
     color: #CDD8F8;
-    span {
-      // color: #fff;
-    }
   }
   .page-route-title {
     color: #B6C7FB ;
     font-weight: bold;
   }
-
   .app-header_actions {
     display: flex;
     margin-left: auto;
@@ -307,35 +255,85 @@ export default {
       }
     }
   }
-
   .btn--toolbar-settings {
     min-width: 0;
     color: $toolbar-button-border;
     background: #141c31;
     width: 81px;
     height: 30px;
-
     padding-right: 1rem;
     padding-left: 1rem;
     border: 1px solid $toolbar-separator-color;
-
     font-family: Nunito Sans;
     font-style: normal;
     font-weight: 600;
     font-size: 12px;
-
     &.active {
       color: $color-1;
       border: 1px solid $color-1;
-
       & > .ring-icon {
         border: 2px solid $color-1;
       }
     }
-
   }
-
   .help-button {
+    display: flex;
+    align-items: center;
+
+    position: relative;
+
+    & > svg:hover > path {
+      fill: #5E6F9F;
+    }
+
+    .help-button-panel {
+      // transform-origin: top right;
+      position: absolute;
+      top: 3rem;
+      right: 0;
+
+      background: #131B30;
+      border: 1px solid #363E51;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+      border-radius: 2px;
+
+      // width: 16rem;
+
+      margin-left: 0;
+
+      .help-button-panel-content {
+        
+        height: 100%; 
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        
+        padding: 1.5rem 1rem;
+
+        > .help-button-panel-content-item {
+          margin: 0;
+          font-family: Nunito Sans;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 14px;
+          line-height: 19px;
+          color: #FFFFFF;
+
+          cursor: pointer;
+
+          display: flex;
+          align-items: center;
+          white-space: nowrap;
+
+        }
+
+        > .help-button-panel-content-item + .help-button-panel-content-item {
+          margin-top: 1rem;
+        }
+      }
+    }
+  }.help-button {
     display: flex;
     align-items: center;
 
@@ -404,10 +402,8 @@ export default {
     font-size: 14px;
     font-weight: bold;
     color: #fff;
-    
     position: relative;
     &.router-link-active {
-      
       color: #B6C7FB;
       &:after {
         content: '';
@@ -420,11 +416,13 @@ export default {
       }
     }
   }
-
   .btn--circle {
     width: 25px;
     height: 25px;
     border: 1px solid #5E6F9F;
     border-radius: 50%;
+  }
+  .cursor-default {
+    cursor: default !important;
   }
 </style>
