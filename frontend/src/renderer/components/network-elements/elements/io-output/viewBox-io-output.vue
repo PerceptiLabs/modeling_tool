0ab4cea34c3a0a7d1cd6f02db21cc5aeef14840c
Anton Bourosu <anton@smartdata.solutions>
2021-11-23 10:33:51 +0000

    Merged PR 1650: Fix issue with statistics weird display
    # Implementation Summary
    -------------------------------
    *Here you can write about how the change affect the users and/or the developers.*
    *You can also add details of **what** was changed, **why** it was changed and **how** it was changed.*
    
    # [Optional] How to review
    -------------------------------
    N/A
    
    # [Optional] Please review the following files
    -------------------------------
    N/A
    
    # What did you do to make the codebase a better place? How did you make the improvements durable
    -------------------------------
    *Here you should describe what improvements you made to codebase while working on the ticket.*
    *Note that the improvements should be proportional in size to the main objective of the ticket*
    
    # Please also:
    - [ ] Include any new files in included_files.txt
    - [ ] Update the story on the board
    - [ ] Announce changes that will affect everyone in #dev in slack
    
    Fix issue with statistics weird display
    
    Related work items: #2581

--- frontend/src/renderer/components/network-elements/elements/io-output/viewBox-io-output.vue
+++ frontend/src/renderer/components/network-elements/elements/io-output/viewBox-io-output.vue
@@ -1,133 +1,124 @@
 <template lang="pug">
   .statistics-box
     .statistics-box_main.statistics-box_col(v-if="currentTab !== 'Global Loss'")
-      template(v-if="sectionTitle === 'Statistics' && currentTab !== 'Performance'")
+      div(:class="`${getChartDisplayClass()}`" v-if="sectionTitle === 'Statistics' && currentTab !== 'Performance'")
         template(v-if="chartData.hasOwnProperty('Accuracy')")
-          .statistics-box_row
-            chart-switch(
-              key="1"
-              chart-label="Accuracy during one epoch"
-              :chart-data="chartData.Accuracy.OverSteps"
-              :custom-color="colorListAccuracy"
-            )
-            chart-switch(
-              key="2"
-              chart-label="Accuracy over all epochs"
-              :chart-data="chartData.Accuracy.OverEpochs"
-              :custom-color="colorListAccuracy"
-            )
+          chart-switch(
+            key="1"
+            chart-label="Accuracy during one epoch"
+            :chart-data="chartData.Accuracy.OverSteps"
+            :custom-color="colorListAccuracy"
+          )
+          chart-switch(
+            key="2"
+            chart-label="Accuracy over all epochs"
+            :chart-data="chartData.Accuracy.OverEpochs"
+            :custom-color="colorListAccuracy"
+          )
         template(v-if="chartData.hasOwnProperty('LossAndAccuracy')")
-          .statistics-box_row
-            chart-switch(
-              key="1"
-              chart-label="Loss over all epochs"
-              :chart-data="chartData.LossAndAccuracy.LossOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
-            chart-switch(
-              key="2"
-              chart-label="Accuracy over all epochs"
-              :chart-data="chartData.LossAndAccuracy.AccOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
+          chart-switch(
+            key="1"
+            chart-label="Loss over all epochs"
+            :chart-data="chartData.LossAndAccuracy.LossOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
+          chart-switch(
+            key="2"
+            chart-label="Accuracy over all epochs"
+            :chart-data="chartData.LossAndAccuracy.AccOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
         template(v-if="chartData.hasOwnProperty('LossAndRSquared')")
-          .statistics-box_row
-            chart-switch(
-              key="1"
-              chart-label="Loss over all epochs"
-              :chart-data="chartData.LossAndRSquared.LossOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
-            chart-switch(
-              key="2"
-              chart-label="R Squared over all epochs"
-              :chart-data="chartData.LossAndRSquared.RSquaredOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
+          chart-switch(
+            key="1"
+            chart-label="Loss over all epochs"
+            :chart-data="chartData.LossAndRSquared.LossOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
+          chart-switch(
+            key="2"
+            chart-label="R Squared over all epochs"
+            :chart-data="chartData.LossAndRSquared.RSquaredOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
         template(v-if="chartData.hasOwnProperty('PvGAndConfusionMatrix')")
-          .statistics-box_row
-            chart-switch(
-              key="3"
-              chart-label="Prediction vs Ground truth"
-              :chart-data="chartData.PvGAndConfusionMatrix.Sample"
-              :custom-color="colorList"
-            )
-            chart-switch(
-              key="4"
-              chart-label="Predictions for Each Class"
-              :chart-data="chartData.PvGAndConfusionMatrix.LastEpoch"
-              :custom-color="colorList"
-            )
+          chart-switch(
+            key="3"
+            chart-label="Prediction vs Ground truth"
+            :chart-data="chartData.PvGAndConfusionMatrix.Sample"
+            :custom-color="colorList"
+          )
+          chart-switch(
+            key="4"
+            chart-label="Predictions for Each Class"
+            :chart-data="chartData.PvGAndConfusionMatrix.LastEpoch"
+            :custom-color="colorList"
+          )
         template(v-if="chartData.hasOwnProperty('PvGAndMAE')")
-          .statistics-box_row
-            chart-switch(
-              key="3"
-              chart-label="Mean Absolute Error over all Epochs"
-              :chart-data="chartData.PvGAndMAE.MAEOverEpochs"
-              :custom-color="colorList"
-            )
-            chart-switch(
-              key="4"
-              chart-label="Prediction vs Ground truth"
-              :chart-data="chartData.PvGAndMAE.Sample"
-              :custom-color="colorList"
-            )
+          chart-switch(
+            key="3"
+            chart-label="Mean Absolute Error over all Epochs"
+            :chart-data="chartData.PvGAndMAE.MAEOverEpochs"
+            :custom-color="colorList"
+          )
+          chart-switch(
+            key="4"
+            chart-label="Prediction vs Ground truth"
+            :chart-data="chartData.PvGAndMAE.Sample"
+            :custom-color="colorList"
+          )
         template(v-if="chartData.hasOwnProperty('PvG')")
-          .statistics-box_row
-            chart-switch(
-              key="3"
-              chart-label="Prediction vs Ground truth"
-              :chart-data="chartData.PvG.Sample"
-              :custom-color="colorList"
-            )
-            chart-switch(
-              key="4"
-              chart-label="Batch Average Prediction vs Ground truth"
-              :chart-data="chartData.PvG.BatchAverage"
-              :custom-color="colorList"
-            )
+          chart-switch(
+            key="3"
+            chart-label="Prediction vs Ground truth"
+            :chart-data="chartData.PvG.Sample"
+            :custom-color="colorList"
+          )
+          chart-switch(
+            key="4"
+            chart-label="Batch Average Prediction vs Ground truth"
+            :chart-data="chartData.PvG.BatchAverage"
+            :custom-color="colorList"
+          )
         template(v-if="chartData.hasOwnProperty('PvGAndImage')")
-          .statistics-box_row
-            chart-switch(
-              key="3"
-              chart-label="Prediction overlayed with Ground truth"
-              :chart-data="chartData.PvGAndImage.Sample"
-              :custom-color="colorList"
-            )
-            chart-switch(
-              key="4"
-              chart-label="Predicted image"
-              :chart-data="chartData.PvGAndImage.Prediction"
-              :custom-color="colorList"
-            )
+          chart-switch(
+            key="3"
+            chart-label="Prediction overlayed with Ground truth"
+            :chart-data="chartData.PvGAndImage.Sample"
+            :custom-color="colorList"
+          )
+          chart-switch(
+            key="4"
+            chart-label="Predicted image"
+            :chart-data="chartData.PvGAndImage.Prediction"
+            :custom-color="colorList"
+          )
         template(v-if="chartData.hasOwnProperty('IoU')")
-          .statistics-box_row
-            chart-switch(
-              key="5"
-              chart-label="IoU during one epoch"
-              :chart-data="chartData.IoU.OverSteps"
-              :custom-color="colorListAccuracy"
-            )
-            chart-switch(
-              key="6"
-              chart-label="IoU over all epochs"
-              :chart-data="chartData.IoU.OverEpochs"
-              :custom-color="colorListAccuracy"
-            ) 
+          chart-switch(
+            key="5"
+            chart-label="IoU during one epoch"
+            :chart-data="chartData.IoU.OverSteps"
+            :custom-color="colorListAccuracy"
+          )
+          chart-switch(
+            key="6"
+            chart-label="IoU over all epochs"
+            :chart-data="chartData.IoU.OverEpochs"
+            :custom-color="colorListAccuracy"
+          ) 
         template(v-if="chartData.hasOwnProperty('IoUAndLoss')")
-          .statistics-box_row
-            chart-switch(
-              key="5"
-              chart-label="Loss over all epochs"
-              :chart-data="chartData.IoUAndLoss.LossOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
-            chart-switch(
-              key="6"
-              chart-label="IoU over all epochs"
-              :chart-data="chartData.IoUAndLoss.IoUOverEpochs"
-              :custom-color="colorListAccuracy"
-            )
+          chart-switch(
+            key="5"
+            chart-label="Loss over all epochs"
+            :chart-data="chartData.IoUAndLoss.LossOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
+          chart-switch(
+            key="6"
+            chart-label="IoU over all epochs"
+            :chart-data="chartData.IoUAndLoss.IoUOverEpochs"
+            :custom-color="colorListAccuracy"
+          )
       template(v-else-if="sectionTitle === 'Statistics' && currentTab === 'Performance'")
         .statistics-box_row
           .statistics-box_col
@@ -148,15 +139,13 @@
               :colorListAccuracy="colorListAccuracy"
               chartTitle="F1"
             )
-      template(v-else)
-        template(v-if="chartData.hasOwnProperty('ViewBox')")
-          .statistics-box_row
-            chart-switch(
-              key="7"
-              chart-label="Output"
-              :chart-data="chartData.ViewBox.Output"
-              :custom-color="colorListAccuracy"
-            )
+      template(v-else-if="chartData.hasOwnProperty('ViewBox')")
+        chart-switch(
+          key="7"
+          chart-label="Output"
+          :chart-data="chartData.ViewBox.Output"
+          :custom-color="colorListAccuracy"
+        )
         
     .statistics-box_main.statistics-box_col(v-if="currentTab === 'Global Loss' && chartData.Loss")
       template(v-if="sectionTitle === 'Statistics'")
@@ -233,6 +222,21 @@ export default {
     }
   },
   methods: {
+    getChartDisplayClass() {
+      const classNamePrefixes = {
+        1: 'one',
+        2: 'two',
+        4: 'four',
+      };
+      let itemsCount = 0;
+      Object.keys(this.chartData).forEach(key => {
+        if (key !== 'ViewBox' && key !== '__ob__') {
+          let itm = this.chartData[key]
+          itemsCount += Object.keys(itm).length;
+        }
+      });
+      return `${classNamePrefixes[itemsCount]}-box-display`;
+    },
     getData() {
       switch (this.currentTab) {
         case 'Global Loss':
@@ -246,3 +250,45 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+.one-box-display {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  > .base-chart:not(.full-view) {
+    width: calc(100% - 5px);
+    height: calc(100% - 5px);
+    margin-bottom: 6px;
+  }
+   > .base-chart:nth-child(2n):not(.full-view) {
+     margin-left: 10px;
+  }
+}
+.two-box-display {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  > .base-chart:not(.full-view) {
+    width: calc(50% - 5px);
+    height: calc(100% - 5px);
+    margin-bottom: 6px;
+  }
+   > .base-chart:nth-child(2n):not(.full-view) {
+     margin-left: 10px;
+  }
+}
+.four-box-display {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  > .base-chart:not(.full-view) {
+    width: calc(50% - 5px);
+    height: calc(50% - 5px);
+    margin-bottom: 6px;
+  }
+   > .base-chart:nth-child(2n):not(.full-view) {
+     margin-left: 10px;
+  }
+}
+
+</style>
